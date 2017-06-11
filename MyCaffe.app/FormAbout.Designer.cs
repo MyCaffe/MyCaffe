23ecabb1c49fe4bdaacdab063c395faf34f6afef
Dave Brown <daveb@signalpop.com>
2017-06-10 17:49:47 -0700

    Updated logo

--- MyCaffe.app/FormAbout.Designer.cs
+++ MyCaffe.app/FormAbout.Designer.cs
@@ -121,6 +121,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(252, 273);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
