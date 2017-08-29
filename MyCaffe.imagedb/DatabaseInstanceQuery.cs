9b0f91791104623a54ce8460fe94abefaced4298
Dave Brown <daveb@signalpop.com>
2017-08-28 22:25:05 -0700

    Added documentation.

--- MyCaffe.imagedb/DatabaseInstanceQuery.cs
+++ MyCaffe.imagedb/DatabaseInstanceQuery.cs
@@ -7,12 +7,22 @@ using System.Threading.Tasks;
 
 namespace MyCaffe.imagedb
 {
+    /// <summary>
+    /// The DatabaseInstanceQuery class is used to find all installed instances of SQL on a given machine.
+    /// </summary>
     public class DatabaseInstanceQuery
     {
+        /// <summary>
+        /// The constructor.
+        /// </summary>
         public DatabaseInstanceQuery()
         {
         }
 
+        /// <summary>
+        /// Returns a list of the SQL instances as a string.
+        /// </summary>
+        /// <returns>The list of SQL instances is returned.</returns>
         public static string GetInstancesAsText()
         {
             List<string> rgstr = GetInstances();
@@ -26,6 +36,10 @@ namespace MyCaffe.imagedb
             return strOut;
         }
 
+        /// <summary>
+        /// Returns a list of the SQL instances as a list of strings.
+        /// </summary>
+        /// <returns>A list of SQL instance strings is returned.</returns>
         public static List<string> GetInstances()
         {
             RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
