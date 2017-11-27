59d95ab215505687d56a7d6d970581136ed98708
Dave Brown <daveb@signalpop.com>
2017-11-26 21:49:21 -0800

    Maintenance release 0.9.0.378

--- MyCaffe.test/Properties/AssemblyInfo.cs
+++ MyCaffe.test/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.0.372")]
-[assembly: AssemblyFileVersion("0.9.0.372")]
+[assembly: AssemblyVersion("0.9.0.378")]
+[assembly: AssemblyFileVersion("0.9.0.378")]
