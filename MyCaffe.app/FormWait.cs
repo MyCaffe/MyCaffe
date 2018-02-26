cba19b837a03365c891b52a9b3ccc47927888eea
Dave Brown <daveb@signalpop.com>
2018-02-25 19:43:26 -0800

    Updated comment.

--- MyCaffe.app/FormWait.cs
+++ MyCaffe.app/FormWait.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace MyCaffe.app
 {
+    /// <summary>
+    /// Wait form used to wait for the application to initialize.
+    /// </summary>
     public partial class FormWait : Form
     {
         public FormWait()
