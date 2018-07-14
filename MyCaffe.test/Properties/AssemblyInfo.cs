d767632dd7fafa64e97d7084597e918cab39d856
ZoroDerVonCodier <windatwall@hotmail.com>
2018-07-13 23:59:51 -0700

    beta release 0.9.2.122 ready

--- MyCaffe.test/Properties/AssemblyInfo.cs
+++ MyCaffe.test/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.2.121")]
-[assembly: AssemblyFileVersion("0.9.2.121")]
+[assembly: AssemblyVersion("0.9.2.122")]
+[assembly: AssemblyFileVersion("0.9.2.122")]
