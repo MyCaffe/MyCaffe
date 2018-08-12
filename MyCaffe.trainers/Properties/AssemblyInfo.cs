069d193b50d52d16c06e43899a2f0680f0659a14
ZoroDerVonCodier <windatwall@hotmail.com>
2018-08-12 09:04:24 -0700

    Added initial A2C support.

---
+++ MyCaffe.trainers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyCaffe.trainers")]
+[assembly: AssemblyDescription("MyCaffe - Deep Learning for Windows Developers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SignalPop LLC")]
+[assembly: AssemblyProduct("MyCaffe")]
+[assembly: AssemblyCopyright("Copyright © 2016-2018 SignalPop and Contributors (see LICENSE). All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("53a34e74-35e4-4e30-888b-cd0705a41bfe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.2.136")]
+[assembly: AssemblyFileVersion("0.9.2.136")]
