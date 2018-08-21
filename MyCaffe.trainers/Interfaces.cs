df218f411ea5f0340f50516da7145403089aac9b
ZoroDerVonCodier <windatwall@hotmail.com>
2018-08-21 11:14:45 -0700

    Simplified.

--- MyCaffe.trainers/Interfaces.cs
+++ MyCaffe.trainers/Interfaces.cs
@@ -26,26 +26,6 @@ namespace MyCaffe.trainers
     }
 
     /// <summary>
-    /// Defines the operating mode.
-    /// </summary>
-    public enum TRAINING_MODE
-    {
-        /// <summary>
-        /// Determines the training mode based on the properties used.  If only one GPU is used, the SINGLE_INSTANCE mode is used,
-        /// otherwise the MULTI_INSTANCE mode is used for multi-gpu configurations.
-        /// </summary>
-        USE_PROPERTIES,
-        /// <summary>
-        /// Run in singlemode where there is only one trainer and the MyCaffeControl passed in acts as the local net.
-        /// </summary>
-        SINGLE_INSTANCE,
-        /// <summary>
-        /// Run in multi-mode where there are multiple trainers and the MyCaffeControl passed in acts as the global net.
-        /// </summary>
-        MULTI_INSTANCE
-    }
-
-    /// <summary>
     /// The IXMyCaffeCustomTrainer interface is used by the MyCaffeCustomTraininer components that
     /// provide various training techniques such as Reinforcement Training.
     /// </summary>
@@ -56,8 +36,7 @@ namespace MyCaffe.trainers
         /// </summary>
         /// <remarks>Use the ProeprtySet object to easily parse the key-value pair properties.</remarks>
         /// <param name="strProperties"></param>
-        /// <param name="mode">Specifies the training mode to use, either A2C (single trainer) or A3C (multi trainer).</param>
-        void Initialize(string strProperties, TRAINING_MODE mode);
+        void Initialize(string strProperties);
         /// <summary>
         /// Clean-up the trainer by releasing all resources used.
         /// </summary>
@@ -87,19 +66,15 @@ namespace MyCaffe.trainers
         /// Train the network using the training technique implemented by this trainer.
         /// </summary>
         /// <param name="mycaffe">Specifies an instance to the MyCaffeControl component.</param>
-        /// <param name="log">Specifies the output Log object.</param>
-        /// <param name="evtCancel">Specifies the cancel event used to halt training.</param>
         /// <param name="nIterationOverride">Specifies the iteration override if any.</param>
         /// <param name="step">Specifies whether or not to step the training for debugging.</param>
-        void Train(Component mycaffe, Log log, CancelEvent evtCancel, int nIterationOverride, TRAIN_STEP step);
+        void Train(Component mycaffe, int nIterationOverride, TRAIN_STEP step);
         /// <summary>
         /// Test the network using the testing technique implemented by this trainer.
         /// </summary>
         /// <param name="mycaffe">Specifies an instance to the MyCaffeControl component.</param>
-        /// <param name="log">Specifies the output Log object.</param>
-        /// <param name="evtCancel">Specifies the cancel event used to halt training.</param>
         /// <param name="nIterationOverride">Specifies the iteration override if any.</param>
-        void Test(Component mycaffe, Log log, CancelEvent evtCancel, int nIterationOverride);
+        void Test(Component mycaffe, int nIterationOverride);
         /// <summary>
         /// Returns the global rewards.
         /// </summary>
