0408f0a133b6c2ef34cf4bdbb571e5a6b210bb39
ZoroDerVonCodier <windatwall@hotmail.com>
2018-08-21 11:15:23 -0700

    Reworked.

--- MyCaffe.trainers/Memory.cs
+++ MyCaffe.trainers/Memory.cs
@@ -10,8 +10,23 @@ namespace MyCaffe.trainers
     /// <summary>
     /// Specifies the memory making up an experience.
     /// </summary>
-    class Memory<T> : GenericList<MemoryItem>
+    class Memory<T, T2> : GenericList<MemoryItem>
     {
+        T2 m_tOne = Utility.ConvertVal<T2>(1.0);
+        T2 m_tZero = Utility.ConvertVal<T2>(0.0);
+
+        public enum STATE_TYPE
+        {
+            OLD,
+            NEW
+        }
+
+        public enum VALUE_TYPE
+        {
+            VALUE,
+            ONEHOT
+        }
+
         /// <summary>
         /// The constructor.
         /// </summary>
@@ -20,59 +35,98 @@ namespace MyCaffe.trainers
         }
 
         /// <summary>
-        /// Get all targets in memory as an array of the base type.
+        /// Clone the memory into a new memory.
         /// </summary>
-        /// <returns>The array of target values is returned.</returns>
-        public T[] GetTargets(int nMax)
+        /// <param name="bClear">If <i>true</i>, clear the original memory.</param>
+        /// <returns>The cloned memory is returned.</returns>
+        public Memory<T, T2> Clone(bool bClear)
         {
-            List<T> rg = new List<T>();
+            Memory<T, T2> col = new Memory<T, T2>();
 
-            for (int i = 0; i < m_rgItems.Count && i < nMax; i++)
-            {
-                T fVal = Utility.ConvertVal<T>(m_rgItems[i].Target);
-                rg.Add(fVal);
-            }
+            col.m_rgItems.AddRange(m_rgItems);
 
-            return rg.ToArray();
+            if (bClear)
+                m_rgItems.Clear();
+
+            return col;
         }
 
         /// <summary>
-        /// Get all actions in memory as an array of the base type.
+        /// Get all states as a list of Datums.
         /// </summary>
-        /// <returns>The array of action values is returned.</returns>
-        public T[] GetActions(int nMax)
+        /// <param name="type">Specifies the information type to retrieve.</param>
+        /// <returns>The list of datums is returned.</returns>
+        public List<Datum> GetStates(STATE_TYPE type)
         {
-            List<T> rg = new List<T>();
+            List<Datum> rg = new List<Datum>();
 
-            for (int i = 0; i < m_rgItems.Count && i < nMax; i++)
+            for (int i = 0; i < m_rgItems.Count; i++)
             {
-                T fVal = Utility.ConvertVal<T>(m_rgItems[i].Action);
-                rg.Add(fVal);
+                if (type == STATE_TYPE.OLD)
+                    rg.Add(new Datum(m_rgItems[i].State0.Data));
+                else
+                    rg.Add(m_rgItems[i].State1 == null ? null : new Datum(m_rgItems[i].State1.Data));
             }
 
-            return rg.ToArray();
+            return rg;
         }
 
         /// <summary>
-        /// Return the actions as a vector of one-hot-vectors each of size nActionCount.
+        /// Get all actions in memory as an array of values or one hot vectors.
         /// </summary>
-        /// <param name="nMax">Specifies the maximum number of items to return.</param>
-        /// <param name="nActionCount">Specifies the action count of each one-hot-vector of the batch of one-hot-vectors.</param>
-        /// <returns>The set of one-hot-vectors is returned as an array.</returns>
-        public T[] GetActionOneHot(int nMax, int nActionCount)
+        /// <param name="type">Specifies the type value or one-hot-vector.</param>
+        /// <returns>The array of action values or one hot vectors is returned.</returns>
+        public T2[] GetActions(VALUE_TYPE type)
         {
-            T tOne = (T)Convert.ChangeType(1.0, typeof(T));
-            T tZero = (T)Convert.ChangeType(0.0, typeof(T));
-            List<T> rg = new List<T>();
+            List<T2> rg = new List<T2>();
 
-            for (int i = 0; i < m_rgItems.Count && i < nMax; i++)
+            for (int i = 0; i < m_rgItems.Count; i++)
             {
                 int nAction = m_rgItems[i].Action;
 
-                for (int j = 0; j < nActionCount; j++)
+                if (type == VALUE_TYPE.ONEHOT)
                 {
-                    rg.Add((j == nAction) ? tOne : tZero);
+                    float[] rgOneHot = new float[m_rgItems[i].State0.ActionCount];
+                    rgOneHot[nAction] = 1;
+
+                    rg.AddRange(Utility.ConvertVec<T2>(rgOneHot));
                 }
+                else
+                {
+                    rg.Add(Utility.ConvertVal<T2>((double)nAction));
+                }
+            }
+
+            return rg.ToArray();
+        }
+
+        /// <summary>
+        /// Get all rewards in memory as an array of base types.
+        /// </summary>
+        /// <returns>The array of rewards is returned.</returns>
+        public T2[] GetRewards()
+        {
+            List<T2> rg = new List<T2>();
+
+            for (int i = 0; i < m_rgItems.Count; i++)
+            {
+                rg.Add(Utility.ConvertVal<T2>(m_rgItems[i].Reward));
+            }
+
+            return rg.ToArray();
+        }
+
+        /// <summary>
+        /// Get all state masks (0 for done, 1 for not done) in memory as an array of base types.
+        /// </summary>
+        /// <returns>The array of state masks is returned.</returns>
+        public T2[] GetStateMask()
+        {
+            List<T2> rg = new List<T2>();
+
+            for (int i = 0; i < m_rgItems.Count; i++)
+            {
+                rg.Add(m_rgItems[i].State1 == null ? m_tZero : m_tOne);
             }
 
             return rg.ToArray();
