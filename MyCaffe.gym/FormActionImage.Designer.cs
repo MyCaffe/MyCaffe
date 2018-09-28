2e506bf17d193d5dc71f564f174be8809eb2834d
ZoroDerVonCodier <daveb@signalpop.com>
2018-09-27 21:49:32 -0700

    Now center image.

--- MyCaffe.gym/FormActionImage.Designer.cs
+++ MyCaffe.gym/FormActionImage.Designer.cs
@@ -37,6 +37,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(156, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
