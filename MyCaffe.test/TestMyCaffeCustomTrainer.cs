6550b7e69a7f0de163379470e9ebe15c4819a622
ZoroDerVonCodier <daveb@signalpop.com>
2018-09-27 21:53:38 -0700

    Added ATARI gym support.

--- MyCaffe.test/TestMyCaffeCustomTrainer.cs
+++ MyCaffe.test/TestMyCaffeCustomTrainer.cs
@@ -73,12 +73,31 @@ namespace MyCaffe.test
                 test.Dispose();
             }
         }
+
+        [TestMethod]
+        public void Train_PGMT_AtariWithOutUi()
+        {
+            MyCaffeCustomTrainerTest test = new MyCaffeCustomTrainerTest();
+
+            try
+            {
+                foreach (IMyCaffeCustomTrainerTest t in test.Tests)
+                {
+                    t.TrainAtariPG(false, 100);
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
     }
 
 
     interface IMyCaffeCustomTrainerTest : ITest
     {
         void TrainCartPolePG(bool bShowUi, string strTrainerType, int nIterations = 1000);
+        void TrainAtariPG(bool bShowUi, int nIterations = 1000);
     }
 
     class MyCaffeCustomTrainerTest : TestBase
@@ -161,12 +180,61 @@ namespace MyCaffe.test
             mycaffe.Dispose();
         }
 
-        private ProjectEx getReinforcementProject(IXMyCaffeGym igym, int nIterations)
+        public void TrainAtariPG(bool bShowUi, int nIterations = 1000)
+        {
+            m_evtCancel.Reset();
+
+            GymCollection col = new GymCollection();
+            col.Load();
+            IXMyCaffeGym igym = col.Find("ATARI");
+            DATA_TYPE dt = DATA_TYPE.BLOB;
+
+            m_log.WriteHeader("Test Training Atari for " + nIterations.ToString("N0") + " iterations.");
+            MyCaffeControl<T> mycaffe = new MyCaffeControl<T>(m_settings, m_log, m_evtCancel);
+            MyCaffeAtariTrainer trainer = new MyCaffeAtariTrainer();
+            ProjectEx project = getReinforcementProject(igym, nIterations, dt);
+            DatasetDescriptor ds = trainer.GetDatasetOverride(0);
+            string strRom = getRomPath("pong.bin");
+
+            m_log.CHECK(ds != null, "The MyCaffeAtariTrainer should return its dataset override returned by the Gym that it uses.");
+
+            // load the project to train (note the project must use the MemoryDataLayer for input).
+            mycaffe.Load(Phase.TRAIN, project, IMGDB_LABEL_SELECTION_METHOD.NONE, IMGDB_IMAGE_SELECTION_METHOD.NONE, false, null, (ds == null) ? true : false);
+
+            // Train the network using the custom trainer
+            //  - Iterations (maximum frames cumulative across all threads) = 1000 (normally this would be much higher such as 500,000)
+            //  - Learning rate = 0.001 (defined in solver.prototxt)
+            //  - Mini Batch Size = 10 (defined in train_val.prototxt for MemoryDataLayer)
+            //
+            //  - TraingerType = 'PG.MT' ('PG.MT' = use multi-threaded Policy Gradient trainer)
+            //  - RewardType = MAX (display the maximum rewards received, a setting of VAL displays the actual reward received)
+            //  - Gamma = 0.99 (discounting factor)
+            //  - Threads = 1 (only use 1 thread if multi-threading is supported)
+            //  - GameROM = 'path to game ROM'
+            trainer.Initialize("TrainerType=PG.MT;RewardType=MAX;Gamma=0.99;Init1=10;GameROM=" + strRom, null);
+
+            if (bShowUi)
+                trainer.OpenUi();
+
+            trainer.Train(mycaffe, nIterations);
+            trainer.CleanUp();
+
+            // Release the mycaffe resources.
+            mycaffe.Dispose();
+        }
+
+        private ProjectEx getReinforcementProject(IXMyCaffeGym igym, int nIterations, DATA_TYPE dt = DATA_TYPE.VALUES)
         {
             ProjectEx p = new ProjectEx("test");
 
             string strModelFile = getTestPath("\\MyCaffe\\test_data\\models\\reinforcement\\cartpole\\train_val.prototxt");
             string strSolverFile = getTestPath("\\MyCaffe\\test_data\\models\\reinforcement\\cartpole\\solver.prototxt");
+
+            if (dt == DATA_TYPE.BLOB)
+            {
+                strModelFile = getTestPath("\\MyCaffe\\test_data\\models\\reinforcement\\atari\\train_val.prototxt");
+                strSolverFile = getTestPath("\\MyCaffe\\test_data\\models\\reinforcement\\atari\\solver.prototxt");
+            }
 
             RawProto protoM = RawProtoFile.LoadFromFile(strModelFile);
             p.ModelDescription = protoM.ToString();
@@ -176,9 +244,14 @@ namespace MyCaffe.test
             iter.Value = nIterations.ToString();
 
             p.SolverDescription = protoS.ToString();
-            p.SetDataset(igym.GetDataset(DATA_TYPE.VALUES));
+            p.SetDataset(igym.GetDataset(dt));
 
             return p;
+        }
+
+        private string getRomPath(string strRom)
+        {
+            return getTestPath("\\MyCaffe\\test_data\\roms\\" + strRom);
         }
     }
 
@@ -210,7 +283,7 @@ namespace MyCaffe.test
             rgdfInit.Add(m_properties.GetPropertyAsDouble("Init1", 10));
             rgdfInit.Add(m_properties.GetPropertyAsDouble("Init2", 0));
 
-            m_igym.Initialize(m_log, rgdfInit.ToArray());
+            m_igym.Initialize(m_log, null, rgdfInit.ToArray());
 
             m_sw.Start();
         }
@@ -254,12 +327,129 @@ namespace MyCaffe.test
 
             Bitmap bmpAction;
             Bitmap bmp = m_igym.Render(512, 512, out bmpAction);
-            Observation obs = new Observation(bmpAction, state.Item1.ToArray(), state.Item2, state.Item3);
+            Observation obs = new Observation(null, bmpAction, state.Item1.ToArray(), state.Item2, state.Item3);
 
             double[] rgState = Observation.GetValues(obs.State, m_bNormalizeInput);
             e.State = new StateBase(m_igym.GetActionSpace().Count());
             e.State.Reward = obs.Reward;
             e.State.Data = new SimpleDatum(true, rgState.Length, 1, 1, -1, DateTime.Now, null, rgState.ToList(), 0, false, 0);
+            e.State.Done = obs.Done;
+            e.State.IsValid = true;
+
+            if (m_gymui != null && m_nUiId >= 0)
+            {
+                m_gymui.Render(m_nUiId, obs);
+                Thread.Sleep(m_igym.UiDelay);
+            }
+
+            if (m_sw.Elapsed.TotalMilliseconds > 1000)
+            {
+                double dfPct = (double)GlobalEpisodeCount / (double)GlobalEpisodeMax;
+                e.OutputLog.Progress = dfPct;
+                e.OutputLog.WriteLine("(" + dfPct.ToString("P") + ") Global Episode #" + GlobalEpisodeCount.ToString() + "  Global Reward = " + GlobalRewards.ToString() + " Exploration Rate = " + ExplorationRate.ToString("P") + " Optimal Selection Rate = " + OptimalSelectionRate.ToString("P"));
+                m_sw.Restart();
+            }
+
+            return true;
+        }
+
+        protected override void openUi()
+        {
+            m_gymui = new MyCaffeGymUiProxy(new InstanceContext(this));
+            m_gymui.Open();
+            m_nUiId = m_gymui.OpenUi(m_strName, m_nUiId);
+        }
+
+        public void Closing()
+        {
+            m_nUiId = -1;
+            m_gymui.Close();
+            m_gymui = null;
+        }
+    }
+
+    class MyCaffeAtariTrainer : MyCaffeTrainerRL, IXMyCaffeGymUiCallback
+    {
+        Stopwatch m_sw = new Stopwatch();
+        IXMyCaffeGym m_igym;
+        Log m_log;
+        int m_nUiId = -1;
+        MyCaffeGymUiProxy m_gymui = null;
+        string m_strName = "ATARI";
+        GymCollection m_colGyms = new GymCollection();
+        DatasetDescriptor m_ds;
+
+        public MyCaffeAtariTrainer()
+            : base()
+        {
+            m_colGyms.Load();
+        }
+
+        protected override void initialize(InitializeArgs e)
+        {
+            m_igym = m_colGyms.Find(m_strName);
+            m_log = e.OutputLog;
+
+            List<double> rgdfInit = new List<double>();
+            rgdfInit.Add(m_properties.GetPropertyAsDouble("Init1", 10));
+            rgdfInit.Add(m_properties.GetPropertyAsDouble("Init2", 0));
+
+            string strROM = m_properties.GetProperty("GameROM");
+            m_igym.Initialize(m_log, strROM, rgdfInit.ToArray());
+
+            m_sw.Start();
+        }
+
+        protected override void shutdown()
+        {
+            if (m_igym != null)
+            {
+                m_igym.Close();
+                m_igym = null;
+            }
+        }
+
+        protected override void dispose()
+        {
+            base.dispose();
+        }
+
+        protected override string name
+        {
+            get { return "RL.Trainer"; }
+        }
+
+        protected override DatasetDescriptor get_dataset_override(int nProjectID)
+        {
+            if (m_igym == null)
+                m_igym = m_colGyms.Find(m_strName);
+
+            m_ds = m_igym.GetDataset(DATA_TYPE.BLOB);
+
+            return m_ds;
+        }
+
+        protected override bool getData(GetDataArgs e)
+        {
+            Tuple<State, double, bool> state = null;
+
+            if (e.Reset)
+                state = m_igym.Reset();
+
+            if (e.Action >= 0)
+                state = m_igym.Step(e.Action);
+
+            Bitmap bmpAction;
+            Bitmap bmp = m_igym.Render(512, 512, out bmpAction);
+            Observation obs = new Observation(bmp, bmpAction, state.Item1.ToArray(), state.Item2, state.Item3);
+
+            int nC = m_ds.TestingSource.ImageChannels;
+            int nH = m_ds.TestingSource.ImageHeight;
+            int nW = m_ds.TestingSource.ImageWidth;
+
+            e.State = new StateBase(m_igym.GetActionSpace().Count());
+            e.State.Reward = obs.Reward;
+            e.State.Data = ImageData.GetImageData(obs.Image, nC, false, 0);
             e.State.Done = obs.Done;
             e.State.IsValid = true;
 
