0e8a4a376bc03e3f806afe36d8504232b74ec801
ZoroDerVonCodier <daveb@signalpop.com>
2018-09-30 22:53:58 -0700

    Moved DirectBitmap to basecode

--- MyCaffe.gym/Gyms/AtariGym.cs
+++ MyCaffe.gym/Gyms/AtariGym.cs
@@ -103,7 +103,7 @@ namespace MyCaffe.gym
             if (properties == null)
                 throw new Exception("The properties must be specified with the 'GameROM' set the the Game ROM file path.");
 
-            string strROM = properties.GetProperty("GameROM");
+            string strROM = Utility.Replace(properties.GetProperty("GameROM"), '~', ' ');
             if (!File.Exists(strROM))
                 throw new Exception("Could not find the game ROM file specified '" + strROM + "'!");
 
@@ -137,6 +137,11 @@ namespace MyCaffe.gym
                 gym.Initialize(m_log, properties);
 
             return gym;
+        }
+
+        public bool RequiresDisplayImage
+        {
+            get { return true; }
         }
 
         public DATA_TYPE SelectedDataType
@@ -313,13 +318,23 @@ namespace MyCaffe.gym
             return new Tuple<State, double, bool>(new AtariState(), dfReward, m_ale.GameOver);
         }
 
-        public DatasetDescriptor GetDataset(DATA_TYPE dt)
+        public DatasetDescriptor GetDataset(DATA_TYPE dt, Log log = null)
         {
             if (dt == DATA_TYPE.DEFAULT)
                 dt = DATA_TYPE.BLOB;
 
             if (dt != DATA_TYPE.BLOB)
-                throw new Exception("This gym only supports the BLOB data type at this time.");
+            {
+                if (log == null)
+                    log = m_log;
+
+                if (log != null)
+                    log.WriteLine("WARNING: This gym only supports the BLOB type, the datatype will be changed to BLOB.");
+                else
+                    throw new Exception("This gym only supports the BLOB type.");
+
+                dt = DATA_TYPE.BLOB;
+            }
 
             int nH = 80;
             int nW = 80;
@@ -354,57 +369,6 @@ namespace MyCaffe.gym
         {
             List<Tuple<double, double, double, bool>> rg = new List<Tuple<double, double, double, bool>>();
             return rg.ToArray();
-        }
-    }
-
-    /// <summary>
-    /// See https://stackoverflow.com/questions/24701703/c-sharp-faster-alternatives-to-setpixel-and-getpixel-for-bitmaps-for-windows-f
-    /// </summary>
-    public class DirectBitmap : IDisposable
-    {
-        public Bitmap Bitmap { get; private set; }
-        public Int32[] Bits { get; private set; }
-        public bool Disposed { get; private set; }
-        public int Height { get; private set; }
-        public int Width { get; private set; }
-
-        protected GCHandle BitsHandle { get; private set; }
-
-        public DirectBitmap(int width, int height)
-        {
-            Width = width;
-            Height = height;
-            Bits = new Int32[width * height];
-            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
-            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
-        }
-
-        public void SetPixel(int x, int y, Color colour)
-        {
-            int index = x + (y * Width);
-            int col = colour.ToArgb();
-
-            Bits[index] = col;
-        }
-
-        public Color GetPixel(int x, int y)
-        {
-            int index = x + (y * Width);
-            int col = Bits[index];
-            Color result = Color.FromArgb(col);
-
-            return result;
-        }
-
-        public void Dispose()
-        {
-            if (Disposed) return;
-            Disposed = true;
-
-            if (Bitmap != null)
-                Bitmap.Dispose();
-
-            BitsHandle.Free();
         }
     }
 }
