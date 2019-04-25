a4f32706b7c5105408b226df8c00eab45621e760
ZoroDerVonCodier <daveb@signalpop.com>
2019-04-24 22:33:41 -0700

    Added to better view string arrays.

---
+++ MyCaffe/param.ui/CsvConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCaffe.param.ui
+{
+    /// <summary>
+    /// The CsvConverter is used to display string lists on a single line in a property grid.
+    /// </summary>
+    public class CsvConverter : TypeConverter
+    {
+        /// <summary>
+        /// Overrides the ConvertTo method of TypeConverter.
+        /// </summary>
+        /// <param name="context">Specifies the context.</param>
+        /// <param name="culture">Specifies the culture.</param>
+        /// <param name="value">Specifies the value.</param>
+        /// <param name="destinationType">Specifies the distination type.</param>
+        /// <returns>The converted type is returned.</returns>
+        public override object ConvertTo(ITypeDescriptorContext context,
+           CultureInfo culture, object value, Type destinationType)
+        {
+            List<String> v = value as List<String>;
+
+            if (destinationType == typeof(string))
+            {
+                return String.Join(",", v.ToArray());
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
