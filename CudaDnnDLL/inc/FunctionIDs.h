565c076c06e49e306fc1dc08866da827f69b0279
ZoroDerVonCodier <daveb@signalpop.com>
2019-05-23 15:12:20 -0700

    Added CHANNEL_MIN

--- CudaDnnDLL/inc/FunctionIDs.h
+++ CudaDnnDLL/inc/FunctionIDs.h
@@ -217,6 +217,7 @@ const int CUDA_FN_COL2IM_ND = 283;
 
 const int CUDA_FN_ACCURACY_FWD = 286;
 
+const int CUDA_FN_CHANNEL_MIN = 289;
 const int CUDA_FN_CHANNEL_MAX = 290;
 const int CUDA_FN_CHANNEL_SUB = 291;
 const int CUDA_FN_CHANNEL_SUM = 292;
