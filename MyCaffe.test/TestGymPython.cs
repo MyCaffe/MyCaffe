ff98af86e4d1f21c57b8381ca12586ac757c2fd2
ZoroDerVonCodier <daveb@signalpop.com>
2019-06-16 07:50:36 -0700

    Added tests for gray.

--- MyCaffe.test/TestGymPython.cs
+++ MyCaffe.test/TestGymPython.cs
@@ -21,7 +21,7 @@ namespace MyCaffe.test
             MyCaffePythonGym gym = new MyCaffePythonGym();
             Random random = new Random();
 
-            gym.Initialize("ATARI", "AllowNegativeRewards=True;TerminateOnRallyEnd=True;FrameSkip=4;GameROM=C:\\ProgramData\\MyCaffe\\test_data\\roms\\breakout.bin");
+            gym.Initialize("ATARI", "Preprocess=False;AllowNegativeRewards=True;TerminateOnRallyEnd=True;FrameSkip=4;GameROM=C:\\ProgramData\\MyCaffe\\test_data\\roms\\breakout.bin");
 
             string strName = gym.Name;
             Assert.AreEqual(strName, "MyCaffe ATARI");
@@ -44,12 +44,71 @@ namespace MyCaffe.test
 
                 Assert.AreNotEqual(gym.Data, null);
 
+                List<List<List<double>>> rgrgrgData = gym.GetDataAsImage();
+
+                Assert.AreNotEqual(rgrgrgData, null);
+                Assert.AreEqual(rgrgrgData.Count, 80);
+                Assert.AreEqual(rgrgrgData[0].Count, 80);
+                Assert.AreEqual(rgrgrgData[0][0].Count, 3);
+
+                rgrgrgData = gym.GetDataAsImage(true);
+
+                Assert.AreNotEqual(rgrgrgData, null);
+                Assert.AreEqual(rgrgrgData.Count, 80);
+                Assert.AreEqual(rgrgrgData[0].Count, 80);
+                Assert.AreEqual(rgrgrgData[0][0].Count, 1);
+
                 Trace.WriteLine("Reward = " + gym.Reward);
 
                 if (gym.IsTerminal)
                     Trace.WriteLine("TERMINAL = TRUE");
             }
-           
+
+            gym.CloseUi();
+        }
+
+        [TestMethod]
+        public void TestGymGray()
+        {
+            MyCaffePythonGym gym = new MyCaffePythonGym();
+            Random random = new Random();
+
+            gym.Initialize("ATARI", "Preprocess=False;ForceGray=True;AllowNegativeRewards=True;TerminateOnRallyEnd=True;FrameSkip=4;GameROM=C:\\ProgramData\\MyCaffe\\test_data\\roms\\breakout.bin");
+
+            string strName = gym.Name;
+            Assert.AreEqual(strName, "MyCaffe ATARI");
+
+            int[] rgActions = gym.Actions;
+            Assert.AreEqual(rgActions.Length, 3);
+            Assert.AreEqual(rgActions[0], 0);
+            Assert.AreEqual(rgActions[1], 1);
+            Assert.AreEqual(rgActions[2], 2);
+
+            gym.OpenUi();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 0 || gym.IsTerminal)
+                    gym.Reset();
+
+                int nActionIdx = random.Next(rgActions.Length);
+                gym.Step(rgActions[nActionIdx], 1);
+
+                Assert.AreNotEqual(gym.Data, null);
+
+                List<List<List<double>>> rgrgrgData = gym.GetDataAsImage();
+
+                Assert.AreNotEqual(rgrgrgData, null);
+                Assert.AreEqual(rgrgrgData.Count, 80);
+                Assert.AreEqual(rgrgrgData[0].Count, 80);
+                Assert.AreEqual(rgrgrgData[0][0].Count, 1);
+
+                Trace.WriteLine("Reward = " + gym.Reward);
+
+                if (gym.IsTerminal)
+                    Trace.WriteLine("TERMINAL = TRUE");
+            }
+
             gym.CloseUi();
         }
     }
