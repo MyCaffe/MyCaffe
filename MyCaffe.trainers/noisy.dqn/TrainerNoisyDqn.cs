381761c0e36fd0ac473d41e9388b5094121efdd6
ZoroDerVonCodier <daveb@signalpop.com>
2019-06-25 20:53:41 -0700

    Fixed bug with beta transition and added accumulated gradients over minibatch.

--- MyCaffe.trainers/noisy.dqn/TrainerNoisyDqn.cs
+++ MyCaffe.trainers/noisy.dqn/TrainerNoisyDqn.cs
@@ -406,7 +406,7 @@ namespace MyCaffe.trainers.noisy.dqn
                 {
                     double dfBeta = beta_by_frame(nIteration);
                     Tuple<MemoryCollection, int[], float[]> rgRandomSamples = rgMemory.GetSamples(m_random, m_brain.BatchSize, dfBeta);
-                    m_brain.Train(rgRandomSamples, s.ActionCount);
+                    m_brain.Train(nIteration, rgRandomSamples, s.ActionCount);
 
                     if (nIteration % m_nTrainingUpdateFreq == 0)
                         m_brain.UpdateTargetModel();
@@ -461,6 +461,10 @@ namespace MyCaffe.trainers.noisy.dqn
         Blob<T> m_blobDone = null;
         Blob<T> m_blobLoss = null;
         Blob<T> m_blobWeights = null;
+        BlobCollection<T> m_colAccumulatedGradients = new BlobCollection<T>();
+        bool m_bUseAcceleratedTraining = false;
+        double m_dfLearningRate;
+        int m_nMiniBatch = 10;
         float m_fGamma = 0.99f;
         int m_nFramesPerX = 4;
         int m_nStackPerX = 4;
@@ -503,6 +507,14 @@ namespace MyCaffe.trainers.noisy.dqn
 
             m_nActionCount = logits.channels;
 
+            m_nMiniBatch = m_properties.GetPropertyAsInt("MiniBatchOverride", m_nMiniBatch);
+
+            double? dfRate = mycaffe.CurrentProject.GetSolverSettingAsNumeric("base_lr");
+            if (dfRate.HasValue)
+                m_dfLearningRate = dfRate.Value;
+
+            m_bUseAcceleratedTraining = properties.GetPropertyAsBool("UseAcceleratedTraining", false);
+
             m_transformer = m_mycaffe.DataTransformer;
 
             for (int i = 0; i < m_nFramesPerX; i++)
@@ -526,6 +538,9 @@ namespace MyCaffe.trainers.noisy.dqn
             m_memLoss = m_net.FindLastLayer(LayerParameter.LayerType.MEMORY_LOSS) as MemoryLossLayer<T>;
             if (m_memLoss == null)
                 m_mycaffe.Log.FAIL("Missing the expected MEMORY_LOSS layer!");
+
+            m_colAccumulatedGradients = m_net.learnable_parameters.Clone();
+            m_colAccumulatedGradients.SetDiff(0);
         }
 
         private void dispose(ref Blob<T> b)
@@ -549,6 +564,12 @@ namespace MyCaffe.trainers.noisy.dqn
             dispose(ref m_blobDone);
             dispose(ref m_blobLoss);
             dispose(ref m_blobWeights);
+
+            if (m_colAccumulatedGradients != null)
+            {
+                m_colAccumulatedGradients.Dispose();
+                m_colAccumulatedGradients = null;
+            }
 
             if (m_argmax != null)
             {
@@ -728,9 +749,10 @@ namespace MyCaffe.trainers.noisy.dqn
         /// <summary>
         /// Train the model at the current iteration.
         /// </summary>
+        /// <param name="nIteration">Specifies the current iteration.</param>
         /// <param name="rgSamples1">Contains the samples to train the model with.</param>
         /// <param name="nActionCount">Specifies the number of actions in the action set.</param>
-        public void Train(Tuple<MemoryCollection, int[], float[]> rgSamples1, int nActionCount)
+        public void Train(int nIteration, Tuple<MemoryCollection, int[], float[]> rgSamples1, int nActionCount)
         {
             MemoryCollection rgSamples = rgSamples1.Item1;
 
@@ -746,9 +768,18 @@ namespace MyCaffe.trainers.noisy.dqn
 
             setCurrentStateData(m_net, rgSamples);
             m_memLoss.OnGetLoss += m_memLoss_ComputeTdLoss;
-            m_solver.Step(1);
+            m_solver.Step(1, TRAIN_STEP.NONE, true, m_bUseAcceleratedTraining, true, true);
+            m_colAccumulatedGradients.Accumulate(m_mycaffe.Cuda, m_net.learnable_parameters, true);
             m_memLoss.OnGetLoss -= m_memLoss_ComputeTdLoss;
             m_mycaffe.Log.Enable = true;
+
+            if (nIteration % m_nMiniBatch == 0)
+            {
+                m_net.learnable_parameters.CopyFrom(m_colAccumulatedGradients, true);
+                m_colAccumulatedGradients.SetDiff(0);
+                m_dfLearningRate = m_solver.ApplyUpdate(nIteration);
+                m_net.ClearParamDiffs();
+            }
 
             resetNoise(m_net);
             resetNoise(m_netTarget);
