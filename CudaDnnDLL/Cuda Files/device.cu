3f01b6aa13c5886ec3228087f24636acf5939c3b
ZoroDerVonCodier <daveb@signalpop.com>
2019-07-14 16:37:57 -0700

    Adde compute to GPU name

--- CudaDnnDLL/Cuda Files/device.cu
+++ CudaDnnDLL/Cuda Files/device.cu
@@ -102,7 +102,7 @@ long Device<T>::GetDeviceName(int nDevice, LPTSTR* pszDevice)
 		return ERROR_OUTOFMEMORY;
 
 	memset(pDst, 0, sizeof(TCHAR) * 512);
-	_sntprintf(pDst, 511, _T("%s (%0.2lf GB - %s)"), A2T(prop.name), dfGB, (b64Bit && bTcc && bVer) ? _T("P2P on") : _T("P2P off"));
+	_sntprintf(pDst, 511, _T("%s (%0.2lf GB - %s, compute %d.%d)"), A2T(prop.name), dfGB, (b64Bit && bTcc && bVer) ? _T("P2P on") : _T("P2P off"), m_nMajor, m_nMinor);
 	*pszDevice = pDst;
 
 	return lErr;
