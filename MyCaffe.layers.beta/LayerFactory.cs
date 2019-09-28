11f221da3fa1862277f9b690cbe079d75ce4ac42
ZoroDerVonCodier <daveb@signalpop.com>
2019-09-27 20:29:06 -0700

    Moved layer.beta into a dynamic layer project.

---
+++ MyCaffe.layers.beta/LayerFactory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCaffe.basecode;
+using MyCaffe.common;
+using MyCaffe.db.image;
+using MyCaffe.param;
+
+/// <summary>
+/// The MyCaffe.layers.ssd namespace contains all SSD related layers.
+/// </summary>
+namespace MyCaffe.layers.beta
+{
+    /// <summary>
+    /// The LayerFactor is responsible for creating all layers implemented in the MyCaffe.layers.ssd namespace.
+    /// </summary>
+    public class LayerFactory : ILayerCreator
+    {
+        /// <summary>
+        /// Create the layers when using the <i>double</i> base type.
+        /// </summary>
+        /// <param name="cuda">Specifies the connection to the low-level CUDA interfaces.</param>
+        /// <param name="log">Specifies the output log.</param>
+        /// <param name="p">Specifies the layer parameter.</param>
+        /// <param name="evtCancel">Specifies the cancellation event.</param>
+        /// <param name="imgDb">Specifies an interface to the image database, who's use is optional.</param>
+        /// <returns>If supported, the layer is returned, otherwise <i>null</i> is returned.</returns>
+        public Layer<double> CreateDouble(CudaDnn<double> cuda, Log log, LayerParameter p, CancelEvent evtCancel, IXImageDatabase imgDb)
+        {
+            switch (p.type)
+            {
+                case LayerParameter.LayerType.EVENT:
+                    return new EventLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.GRAM:
+                    return new GramLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.GRN:
+                    return new GRNLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.KNN:
+                    return new KnnLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.NORMALIZATION1:
+                    return new Normalization1Layer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.ONEHOT:
+                    return new OneHotLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.UNPOOLING1:
+                    return new UnPoolingLayer1<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.UNPOOLING:
+                    return new UnPoolingLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.SCALAR:
+                    return new ScalarLayer<double>(cuda, log, p);
+
+                case LayerParameter.LayerType.TV_LOSS:
+                    return new TVLossLayer<double>(cuda, log, p);
+
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Create the layers when using the <i>float</i> base type.
+        /// </summary>
+        /// <param name="cuda">Specifies the connection to the low-level CUDA interfaces.</param>
+        /// <param name="log">Specifies the output log.</param>
+        /// <param name="p">Specifies the layer parameter.</param>
+        /// <param name="evtCancel">Specifies the cancellation event.</param>
+        /// <param name="imgDb">Specifies an interface to the image database, who's use is optional.</param>
+        /// <returns>If supported, the layer is returned, otherwise <i>null</i> is returned.</returns>
+        public Layer<float> CreateSingle(CudaDnn<float> cuda, Log log, LayerParameter p, CancelEvent evtCancel, IXImageDatabase imgDb)
+        {
+            switch (p.type)
+            {
+                case LayerParameter.LayerType.EVENT:
+                    return new EventLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.GRAM:
+                    return new GramLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.GRN:
+                    return new GRNLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.KNN:
+                    return new KnnLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.NORMALIZATION1:
+                    return new Normalization1Layer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.ONEHOT:
+                    return new OneHotLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.UNPOOLING1:
+                    return new UnPoolingLayer1<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.UNPOOLING:
+                    return new UnPoolingLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.SCALAR:
+                    return new ScalarLayer<float>(cuda, log, p);
+
+                case LayerParameter.LayerType.TV_LOSS:
+                    return new TVLossLayer<float>(cuda, log, p);
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ MyCaffe.layers.beta/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyCaffe.layers.beta")]
+[assembly: AssemblyDescription("MyCaffe - Deep Learning for Windows Developers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SignalPop LLC")]
+[assembly: AssemblyProduct("MyCaffe")]
+[assembly: AssemblyCopyright("Copyright ©  2019 SignalPop LLC. All rights reserved.")]
+[assembly: AssemblyTrademark("SignalPop")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("db6986db-ab61-4bd5-828e-a8eda0a6f56e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
