d0733183ccc009df7fea9d56cbdabf7d1b1fcdcd
ZoroDerVonCodier <daveb@signalpop.com>
2019-12-09 18:43:56 -0800

    Changed default to V2

--- MyCaffe.app/Properties/Settings.Designer.cs
+++ MyCaffe.app/Properties/Settings.Designer.cs
@@ -397,7 +397,7 @@ namespace MyCaffe.app.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int ImgDbVer {
             get {
                 return ((int)(this["ImgDbVer"]));
