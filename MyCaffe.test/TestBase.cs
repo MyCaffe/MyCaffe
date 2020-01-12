dcdf1f5a4b42fbba4bb4b846bbc96f5eeb989e37
ZoroDerVonCodier <daveb@signalpop.com>
2020-01-12 07:14:15 -0800

    Added depreciated tests failures to SKIPPED.

--- MyCaffe.test/TestBase.cs
+++ MyCaffe.test/TestBase.cs
@@ -133,10 +133,11 @@ namespace MyCaffe.test
                 rgKnownFailures.Add(new Tuple<string, string, string>("TestSimpleTripletLossLayer", "TestGradient", "SKIPPED - Values 0.9923858 and 0.354433 are NOT within the range 0.01 of one another."));
                 rgKnownFailures.Add(new Tuple<string, string, string>("TestContrastiveLossLayer", "TestGradientLegacy", "SKIPPED - Values -0.400000 and 0.158390 are NOT within the range 0.01 of one another."));
 
-//                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQuerySequential2LoadLimit", "SKIPPED - Assert.AreEqual failed. Expected:<1>. Actual:<10>."));
-//                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQuerySequential4LoadLimit", "SKIPPED - Assert.AreEqual failed. Expected:<1>. Actual:<10>."));
-//                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQueryPairLoadLimit", "SKIPPED - Assert.AreEqual failed. Expected:<100>. Actual:<10>."));
-                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestLoadLimitNextSequential", "SKIPPED - currently causes lock-up."));
+                //                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQuerySequential2LoadLimit", "SKIPPED - Assert.AreEqual failed. Expected:<1>. Actual:<10>."));
+                //                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQuerySequential4LoadLimit", "SKIPPED - Assert.AreEqual failed. Expected:<1>. Actual:<10>."));
+                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQueryPairLoadLimit", "SKIPPED - DEPRECIATED: Assert.AreEqual failed. Expected:<100>. Actual:<10>."));
+                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestQueryRandomLoadLimit", "SKIPPED - DEPRECIATED: Assert.AreEqual failed. Expected:<True>. Actual:<False>. ."));
+                rgKnownFailures.Add(new Tuple<string, string, string>("TestMyCaffeImageDatabase", "TestLoadLimitNextSequential", "SKIPPED - DEPRECIATED: currently causes lock-up."));
 
                 rgKnownFailures.Add(new Tuple<string, string, string>("TestConvolutionLayer", "TestNDAgainst2D", "SKIPPED - FLOAT:CAFFE Values 0.251427710056305 and -0.860932171344757 are NOT within the range 0.01 of one another.  The 2d and ND backward results are not the same at 1"));
                 rgKnownFailures.Add(new Tuple<string, string, string>("TestConvolutionLayer", "TestGradient3D", "SKIPPED - FLOAT:CAFFE Values 0.107909 and -1.631164 are NOT within the range 0.0016311 of one another."));
