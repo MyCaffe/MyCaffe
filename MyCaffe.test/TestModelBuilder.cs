02024eb3f385c467fb8e913f695999068f58867f
ZoroDerVonCodier <daveb@signalpop.com>
2020-01-28 15:54:59 -0800

    Added tests for ResNet152, ResNet101 and Siamese versions of each.

--- MyCaffe.test/TestModelBuilder.cs
+++ MyCaffe.test/TestModelBuilder.cs
@@ -23,7 +23,7 @@ namespace MyCaffe.test
     public class TestModelBuilder
     {
         [TestMethod]
-        public void TestCreateSolver_SSDPascal()
+        public void TestSSDPascal_CreateSolver()
         {
             SSDPascalModelBuilderTest test = new SSDPascalModelBuilderTest();
 
@@ -41,7 +41,7 @@ namespace MyCaffe.test
         }
 
         [TestMethod]
-        public void TestCreateDeployModel_SSDPascal()
+        public void TestSSDPascal_CreateDeployModel()
         {
             SSDPascalModelBuilderTest test = new SSDPascalModelBuilderTest();
 
@@ -59,9 +59,226 @@ namespace MyCaffe.test
         }
 
         [TestMethod]
-        public void TestCreateTrainingModel_SSDPascal()
+        public void TestSSDPascal_CreateTrainingModel()
         {
             SSDPascalModelBuilderTest test = new SSDPascalModelBuilderTest();
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateTrainingModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet101_CreateSolver()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateSolver();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet101_CreateDeployModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateDeployModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet101_CreateTrainingModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateTrainingModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_CreateSolver()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateSolver();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_CreateDeployModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateDeployModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_CreateTrainingModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateTrainingModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+
+        [TestMethod]
+        public void TestResNet101_Siamese_CreateSolver()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101, true);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateSolver();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet101_Siamese_CreateDeployModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101, true);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateDeployModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet101_Siamease_CreateTrainingModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET101, true);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateTrainingModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_Siamese_CreateSolver()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152, true);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateSolver();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_Siamease_CreateDeployModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152, true);
+
+            try
+            {
+                foreach (IModelBuilderTest t in test.Tests)
+                {
+                    t.TestCreateDeployModel();
+                }
+            }
+            finally
+            {
+                test.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void TestResNet152_Siamese_CreateTrainingModel()
+        {
+            ResNetModelBuilderTest test = new ResNetModelBuilderTest(ResNetModelBuilder.MODEL.RESNET152, true);
 
             try
             {
@@ -84,6 +301,77 @@ namespace MyCaffe.test
         void TestCreateDeployModel();
     }
 
+    class ResNetModelBuilderTest : TestBase
+    {
+        public ResNetModelBuilderTest(ResNetModelBuilder.MODEL model, bool bSiamese = false, EngineParameter.Engine engine = EngineParameter.Engine.DEFAULT)
+            : base("ResNet Model Builder Test" + ((bSiamese) ? " SIAMESE" : "") + " " + model.ToString(), TestBase.DEFAULT_DEVICE_ID, engine)
+        {
+        }
+
+        protected override ITest create(common.DataType dt, string strName, int nDeviceID, EngineParameter.Engine engine)
+        {
+            if (strName.Contains("SIAMESE"))
+            {
+                if (dt == common.DataType.DOUBLE)
+                    return new ResNetSiameseModelBuilderTest<double>(strName, nDeviceID, engine);
+                else
+                    return new ResNetSiameseModelBuilderTest<float>(strName, nDeviceID, engine);
+            }
+            else
+            {
+                if (dt == common.DataType.DOUBLE)
+                    return new ResNetModelBuilderTest<double>(strName, nDeviceID, engine);
+                else
+                    return new ResNetModelBuilderTest<float>(strName, nDeviceID, engine);
+            }
+        }
+    }
+
+    class ResNetSiameseModelBuilderTest<T> : ModelBuilderTest<T>
+    {
+        ResNetModelBuilder.MODEL m_model;
+
+        public ResNetSiameseModelBuilderTest(string strName, int nDeviceID, EngineParameter.Engine engine)
+            : base(strName, nDeviceID, engine)
+        {
+            if (strName.Contains(ResNetModelBuilder.MODEL.RESNET101.ToString()))
+                m_model = ResNetModelBuilder.MODEL.RESNET101;
+            else
+                m_model = ResNetModelBuilder.MODEL.RESNET152;
+        }
+
+        protected override ModelBuilder create()
+        {
+            List<Tuple<int, bool>> rgIP = new List<Tuple<int, bool>>();
+            rgIP.Add(new Tuple<int, bool>(1024, false));
+            rgIP.Add(new Tuple<int, bool>(512, true));
+            rgIP.Add(new Tuple<int, bool>(10, false));
+            int nBatch = (m_model == ResNetModelBuilder.MODEL.RESNET101) ? 16 : 12;
+            return new ResNetModelBuilder(m_strBaseDir, "CIFAR-10", 3, true, rgIP, true, false, m_model, nBatch, nBatch);
+        }
+    }
+
+    class ResNetModelBuilderTest<T> : ModelBuilderTest<T>
+    {
+        ResNetModelBuilder.MODEL m_model;
+
+        public ResNetModelBuilderTest(string strName, int nDeviceID, EngineParameter.Engine engine)
+            : base(strName, nDeviceID, engine)
+        {
+            if (strName.Contains(ResNetModelBuilder.MODEL.RESNET101.ToString()))
+                m_model = ResNetModelBuilder.MODEL.RESNET101;
+            else
+                m_model = ResNetModelBuilder.MODEL.RESNET152;
+        }
+
+        protected override ModelBuilder create()
+        {
+            List<Tuple<int, bool>> rgIP = new List<Tuple<int, bool>>();
+            rgIP.Add(new Tuple<int, bool>(10, false));
+            return new ResNetModelBuilder(m_strBaseDir, "CIFAR-10", 3, false, rgIP, true, false, m_model);
+        }
+    }
+
     class SSDPascalModelBuilderTest : TestBase
     {
         public SSDPascalModelBuilderTest(EngineParameter.Engine engine = EngineParameter.Engine.DEFAULT)
@@ -94,15 +382,31 @@ namespace MyCaffe.test
         protected override ITest create(common.DataType dt, string strName, int nDeviceID, EngineParameter.Engine engine)
         {
             if (dt == common.DataType.DOUBLE)
-                return new ModelBuilderTest<double>(strName, nDeviceID, engine);
+                return new SSDModelBuilderTest<double>(strName, nDeviceID, engine);
             else
-                return new ModelBuilderTest<float>(strName, nDeviceID, engine);
+                return new SSDModelBuilderTest<float>(strName, nDeviceID, engine);
         }
     }
 
-    class ModelBuilderTest<T> : TestEx<T>, IModelBuilderTest
+    class SSDModelBuilderTest<T> : ModelBuilderTest<T>
     {
-        string m_strBaseDir;
+        public SSDModelBuilderTest(string strName, int nDeviceID, EngineParameter.Engine engine)
+            : base(strName, nDeviceID, engine)
+        {
+            m_engine = engine;
+            m_strBaseDir = TestBase.GetTestPath("\\MyCaffe\\test_data", true, true);
+        }
+
+        protected override ModelBuilder create()
+        {
+            return new SsdPascalModelBuilder(m_strBaseDir);
+        }
+    }
+
+
+    abstract class ModelBuilderTest<T> : TestEx<T>, IModelBuilderTest
+    {
+        protected string m_strBaseDir;
 
         public ModelBuilderTest(string strName, int nDeviceID, EngineParameter.Engine engine)
             : base(strName, null, nDeviceID)
@@ -116,9 +420,11 @@ namespace MyCaffe.test
             base.dispose();
         }
 
+        protected abstract ModelBuilder create();
+
         public void TestCreateSolver()
         {
-            SsdPascalModelBuilder builder = new SsdPascalModelBuilder(m_strBaseDir);
+            ModelBuilder builder = create();
 
             SolverParameter solverParam = builder.CreateSolver();
             RawProto proto = solverParam.ToProto("root");
@@ -132,7 +438,8 @@ namespace MyCaffe.test
 
         public void TestCreateTrainingModel()
         {
-            SsdPascalModelBuilder builder = new SsdPascalModelBuilder(m_strBaseDir);
+            ModelBuilder builder = create();
+
             NetParameter net_param = builder.CreateModel();
             RawProto proto = net_param.ToProto("root");
             string strNet = proto.ToString();
@@ -151,13 +458,14 @@ namespace MyCaffe.test
             CancelEvent evtCancel = new CancelEvent();
             MyCaffeControl<T> mycaffe = new MyCaffeControl<T>(settings, m_log, evtCancel);
 
-//            mycaffe.LoadLite(Phase.TRAIN, strSolver, strNet, null);
+            //            mycaffe.LoadLite(Phase.TRAIN, strSolver, strNet, null);
             mycaffe.Dispose();
         }
 
         public void TestCreateDeployModel()
         {
-            SsdPascalModelBuilder builder = new SsdPascalModelBuilder(m_strBaseDir);
+            ModelBuilder builder = create();
+
             NetParameter net_param = builder.CreateDeployModel();
             RawProto proto = net_param.ToProto("root");
             string strNet = proto.ToString();
@@ -172,7 +480,7 @@ namespace MyCaffe.test
             CancelEvent evtCancel = new CancelEvent();
             MyCaffeControl<T> mycaffe = new MyCaffeControl<T>(settings, m_log, evtCancel);
 
-//            mycaffe.LoadToRun(strNet, null, new BlobShape(1, 3, 300, 300));
+            //            mycaffe.LoadToRun(strNet, null, new BlobShape(1, 3, 300, 300));
             mycaffe.Dispose();
         }
     }
