09aaeea5b1b85c5640897f9ab9a9905f04f93d2a
ZoroDerVonCodier <daveb@signalpop.com>
2020-05-23 20:47:09 -0700

    Added new converter project to convert from MyCaffe and CAFFE models to ONNX models.

---
+++ MyCaffe.converter.onnx/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyCaffe.converter.onnx")]
+[assembly: AssemblyDescription("MyCaffe - Deep Learning for Windows Developers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SignalPop LLC")]
+[assembly: AssemblyProduct("MyCaffe")]
+[assembly: AssemblyCopyright("Copyright © 2020 SignalPop and Contributors (see LICENSE). All rights reserved.")]
+[assembly: AssemblyTrademark("SignalPop")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43b01b30-0f7a-4764-a20b-791b936e6276")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.10.2.285")]
+[assembly: AssemblyFileVersion("0.10.2.285")]
