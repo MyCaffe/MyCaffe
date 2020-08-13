6746b6c206326d0cf5c8dc7e1a8c39ed34806014
ZoroDerVonCodier <daveb@signalpop.com>
2020-08-12 17:36:53 -0700

    Cleaned up SQL use.

--- MyCaffe.app/Program.cs
+++ MyCaffe.app/Program.cs
@@ -14,7 +14,7 @@ namespace MyCaffe.app
         [STAThread]
         static void Main()
         {
-            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+            //SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
