3b0a5eea0c4bfa7017fba886c831b56ab8c374c7
ZoroDerVonCodier <daveb@signalpop.com>
2020-08-31 06:51:50 -0700

    Added key-value support.

--- MyCaffe.basecode/ConnectInfo.cs
+++ MyCaffe.basecode/ConnectInfo.cs
@@ -11,10 +11,34 @@ namespace MyCaffe.basecode
     /// </summary>
     public class ConnectInfo
     {
+        TYPE m_location = TYPE.NONE;
         string m_strServer;
         string m_strUn;
         string m_strPw;
         string m_strDb;
+
+        /// <summary>
+        /// Defines the generic connection location
+        /// </summary>
+        public enum TYPE
+        {
+            /// <summary>
+            /// Specifies no location.
+            /// </summary>
+            NONE,
+            /// <summary>
+            /// Specifies a local connection.
+            /// </summary>
+            LOCAL,
+            /// <summary>
+            /// Specifies a remote connection.
+            /// </summary>
+            REMOTE,
+            /// <summary>
+            /// Specifies a connection to Azure.
+            /// </summary>
+            AZURE
+        }
 
         /// <summary>
         /// The constructor.
@@ -23,12 +47,14 @@ namespace MyCaffe.basecode
         /// <param name="strDb">Specifies the database (default = 'DNN')</param>
         /// <param name="strUn">Specifies the username (default = null)</param>
         /// <param name="strPw">Specifies the password (default = null)</param>
-        public ConnectInfo(string strServer = null, string strDb = "DNN", string strUn = null, string strPw = null)
+        /// <param name="location">Specifies a generic location type.</param>
+        public ConnectInfo(string strServer = null, string strDb = "DNN", string strUn = null, string strPw = null, TYPE location = TYPE.NONE)
         {
             m_strServer = strServer;
             m_strDb = strDb;
             m_strUn = strUn;
             m_strPw = strPw;
+            m_location = location;
         }
 
         /// <summary>
@@ -50,6 +76,9 @@ namespace MyCaffe.basecode
             if (ci.Password != m_strPw)
                 return false;
 
+            if (ci.Location != m_location)
+                return false;
+
             return true;
         }
 
@@ -66,7 +95,16 @@ namespace MyCaffe.basecode
         }
 
         /// <summary>
-        /// Returns the server.
+        /// Get/set the generic location type.
+        /// </summary>
+        public TYPE Location
+        {
+            get { return m_location; }
+            set { m_location = value; }
+        }
+
+        /// <summary>
+        /// Get/set the server.
         /// </summary>
         public string Server
         {
@@ -75,7 +113,7 @@ namespace MyCaffe.basecode
         }
 
         /// <summary>
-        /// Returns the database.
+        /// Get/set the database.
         /// </summary>
         public string Database
         {
@@ -97,6 +135,85 @@ namespace MyCaffe.basecode
         public string Password
         {
             get { return m_strPw; }
+        }
+
+        /// <summary>
+        /// Returns the connection information as a set of key-value pairs.
+        /// </summary>
+        /// <returns>The key-value pair string is returned.</returns>
+        public string ToKeyValuePairs()
+        {
+            string str = "";
+
+            str += "Type\t" + Location.ToString() + ";";
+            str += "Server\t" + Server + ";";
+            str += "Database\t" + Database + ";";
+            str += "Username\t" + Username + ";";
+            str += "Password\t" + Password + ";";
+
+            return str;
+        }
+
+        /// <summary>
+        /// Parses a key-value pair string containing the connection information and returns a ConnectInfo.
+        /// </summary>
+        /// <param name="strKeyValPairs">Specifies the key-value pairs string.</param>
+        /// <returns>The new ConnectInfo object is returned with the connection information.</returns>
+        public static ConnectInfo ParseKeyValuePairs(string strKeyValPairs)
+        {
+            string[] rgstr = strKeyValPairs.Split(';');
+            string strServer = null;
+            string strDatabase = null;
+            string strUsername = null;
+            string strPassword = null;
+            string strType = null;
+            TYPE location = TYPE.NONE;
+
+            foreach (string str1 in rgstr)
+            {
+                if (!string.IsNullOrEmpty(str1))
+                {
+                    string[] rgstr1 = str1.Split('\t');
+                    if (rgstr1.Length != 2)
+                        throw new Exception("ConnectInfo: Invalid key-value pair!");
+
+                    if (rgstr1[0] == "Type")
+                        strType = rgstr1[1];
+
+                    else if (rgstr1[0] == "Server")
+                        strServer = rgstr1[1];
+
+                    else if (rgstr1[0] == "Database")
+                        strDatabase = rgstr1[1];
+
+                    else if (rgstr1[0] == "Username")
+                        strUsername = rgstr1[1];
+
+                    else if (rgstr1[0] == "Password")
+                        strPassword = rgstr1[1];
+                }
+            }
+
+            if (strType == TYPE.AZURE.ToString())
+                location = TYPE.AZURE;
+            else if (strType == TYPE.REMOTE.ToString())
+                location = TYPE.REMOTE;
+            else if (strType == TYPE.LOCAL.ToString())
+                location = TYPE.LOCAL;
+
+            if (strServer.Length == 0)
+                strServer = null;
+
+            if (strDatabase.Length == 0)
+                strDatabase = null;
+
+            if (strUsername.Length == 0)
+                strUsername = null;
+
+            if (strPassword.Length == 0)
+                strPassword = null;
+
+            return new ConnectInfo(strServer, strDatabase, strUsername, strPassword, location);
         }
     }
 }
