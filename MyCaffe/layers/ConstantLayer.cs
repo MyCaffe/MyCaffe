98d6b18d4ff72c812ef967c6fb7584b0525b4d90
ZoroDerVonCodier <daveb@signalpop.com>
2020-10-21 19:50:28 -0700

    Added Constant layer.

---
+++ MyCaffe/layers/ConstantLayer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MyCaffe.basecode;
+using MyCaffe.common;
+using MyCaffe.param;
+
+namespace MyCaffe.layers
+{
+    /// <summary>
+    /// The ConstantLayer provides a layer that just outputs a constant value.
+    /// This layer is initialized with the MyCaffe.param.ConstantParameter.
+    /// </summary>
+    /// <typeparam name="T">Specifies the base type <i>float</i> or <i>double</i>.  Using <i>float</i> is recommended to conserve GPU memory.</typeparam>
+    public class ConstantLayer<T> : Layer<T>
+    {
+        BlobShape m_shape;
+        List<float> m_rgF;
+
+        /// <summary>
+        /// The ConstantLayer constructor.
+        /// </summary>
+        /// <param name="cuda">Specifies the CudaDnn connection to Cuda.</param>
+        /// <param name="log">Specifies the Log for output.</param>
+        /// <param name="p">Specifies the LayerParameter of type Constant with parameter constant_param</param>
+        public ConstantLayer(CudaDnn<T> cuda, Log log, LayerParameter p)
+            : base(cuda, log, p)
+        {
+            m_type = LayerParameter.LayerType.CONSTANT;
+        }
+
+        /// <summary>
+        /// Setup the layer.
+        /// </summary>
+        /// <param name="colBottom">Specifies the input blobs, which are not used.</param>
+        /// <param name="colTop">Specifies the output blobs.</param>
+        public override void LayerSetUp(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+            m_shape = m_param.constant_param.output_shape;
+            m_rgF = m_param.constant_param.values_f;
+
+            if (!string.IsNullOrEmpty(m_param.constant_param.binary_data_file))
+            {
+                m_log.CHECK(File.Exists(m_param.constant_param.binary_data_file), "The 'binary_data_file' specified ('" + m_param.constant_param.binary_data_file + "') could not be found!");
+
+                using (FileStream fs = new FileStream(m_param.constant_param.binary_data_file, FileMode.Open, FileAccess.Read))
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    BlobProto proto = BlobProto.Load(br);
+                    m_rgF = proto.data;
+                }
+            }
+
+            m_log.CHECK_EQ(m_rgF.Count, 1, "Currently, only single constant values are supported!");
+        }
+
+        /// <summary>
+        /// Reshape the layer.
+        /// </summary>
+        /// <param name="colBottom">Specifies the input blobs, which are not used.</param>
+        /// <param name="colTop">Specifies the output blobs which are reshaped to the output_shape.</param>
+        public override void Reshape(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+            colTop[0].Reshape(m_param.constant_param.output_shape);
+        }
+
+        /// <summary>
+        /// Forward computation
+        /// </summary>
+        /// <param name="colBottom">inpub Blob vector - not used.</param>
+        /// <param name="colTop">top output Blob vector (length 1)
+        ///  -# @f$ (N \times C \times H \times W) @f$
+        ///     where NCHW are specified by the 'output_shape'.
+        /// </param>
+        protected override void forward(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+            colTop[0].SetData(m_rgF[0]);
+        }
+
+        /// @brief Not implemented - constant Layers do not perform backward.
+        protected override void backward(BlobCollection<T> colTop, List<bool> rgbPropagateDown, BlobCollection<T> colBottom)
+        {            
+        }
+    }
+}
---
+++ MyCaffe/param/ConstantParameter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyCaffe.basecode;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace MyCaffe.param
+{
+    /// <summary>
+    /// Specifies the parameters for the ConstantLayer.
+    /// </summary>
+    public class ConstantParameter : LayerParameterBase 
+    {
+        BlobShape m_outputShape = new BlobShape();
+        List<float> m_rgF = new List<float>();
+        string m_strBinaryDataFile = null;
+
+        /** @copydoc LayerParameterBase */
+        public ConstantParameter()
+        {
+        }
+
+        /// <summary>
+        /// Specifies the output shape.
+        /// </summary>
+        [Description("Specifies the output shape.")]
+        public BlobShape output_shape
+        {
+            get { return m_outputShape; }
+            set { m_outputShape = value; }
+        }
+
+        /// <summary>
+        /// Specifies a binary data file containing the values to load.
+        /// </summary>
+        /// <remarks>
+        /// The binary data file is in the format:
+        /// int nCount
+        /// float f0
+        /// float f1
+        /// :
+        /// float fn
+        /// </remarks>
+        [Description("Specifies a binary data file containing the values to load.")]
+        public string binary_data_file
+        {
+            get { return m_strBinaryDataFile; }
+            set { m_strBinaryDataFile = value; }
+        }
+
+        /// <summary>
+        /// Specifies a set of float values used to fill the output.  When only one item is specified, all outputs are set to that value.
+        /// </summary>
+        [Description("Specifies a set of float values used to fill the output.  When only one item is specified, all outputs are set to that value.")]
+        public List<float> values_f
+        {
+            get { return m_rgF; }
+            set { m_rgF = value; }
+        }
+
+        /// <summary>
+        /// Load the parameter from a binary reader.
+        /// </summary>
+        /// <param name="br">Specifies the binary reader.</param>
+        /// <param name="bNewInstance">When <i>true</i> a new instance is created (the default), otherwise the existing instance is loaded from the binary reader.</param>
+        /// <returns>Returns an instance of the parameter.</returns>
+        public override object Load(System.IO.BinaryReader br, bool bNewInstance = true)
+        {
+            RawProto proto = RawProto.Parse(br.ReadString());
+            ConstantParameter p = FromProto(proto);
+
+            if (!bNewInstance)
+                Copy(p);
+
+            return p;
+        }
+
+        /// <summary>
+        /// Copy on parameter to another.
+        /// </summary>
+        /// <param name="src">Specifies the parameter to copy.</param>
+        public override void Copy(LayerParameterBase src)
+        {
+            ConstantParameter p = (ConstantParameter)src;
+            m_outputShape = p.output_shape.Clone();
+            m_rgF = new List<float>(p.m_rgF);
+            m_strBinaryDataFile = p.binary_data_file;
+        }
+
+        /// <summary>
+        /// Creates a new copy of this instance of the parameter.
+        /// </summary>
+        /// <returns>A new instance of this parameter is returned.</returns>
+        public override LayerParameterBase Clone()
+        {
+            ConstantParameter p = new ConstantParameter();
+            p.Copy(this);
+            return p;
+        }
+
+        private static string replace(string str, char chTarget, char chReplace)
+        {
+            string strOut = "";
+
+            foreach (char ch in str)
+            {
+                if (ch == chTarget)
+                    strOut += chReplace;
+                else
+                    strOut += ch;
+            }
+
+            return strOut;
+        }
+
+
+        /// <summary>
+        /// Convert the parameter into a RawProto.
+        /// </summary>
+        /// <param name="strName">Specifies the name to associate with the RawProto.</param>
+        /// <returns>The new RawProto is returned.</returns>
+        public override RawProto ToProto(string strName)
+        {
+            RawProtoCollection rgChildren = new RawProtoCollection();
+
+            rgChildren.Add(m_outputShape.ToProto("output_shape"));
+            rgChildren.Add<float>("valuef", m_rgF);
+
+            if (!string.IsNullOrEmpty(m_strBinaryDataFile))
+                rgChildren.Add("binary_data_file", replace(m_strBinaryDataFile, ' ', '~'));
+
+            return new RawProto(strName, "", rgChildren);
+        }
+
+        /// <summary>
+        /// Parses the parameter from a RawProto.
+        /// </summary>
+        /// <param name="rp">Specifies the RawProto to parse.</param>
+        /// <returns>A new instance of the parameter is returned.</returns>
+        public static ConstantParameter FromProto(RawProto rp)
+        {
+            string strVal;
+            ConstantParameter p = new ConstantParameter();
+
+            RawProto shape = rp.FindChild("output_shape");
+            if (shape != null)
+                p.m_outputShape = BlobShape.FromProto(shape);
+
+            p.m_rgF = rp.FindArray<float>("valuef");
+
+            strVal = rp.FindValue("binary_data_file");
+            if (strVal != null)
+                p.m_strBinaryDataFile = replace(strVal, '~', ' ');
+
+            return p;
+        }
+    }
+}
