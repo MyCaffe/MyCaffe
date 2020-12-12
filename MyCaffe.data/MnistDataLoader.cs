07d8259edd6af49e1f6b728bfa9fd1414c441207
ZoroDerVonCodier <daveb@signalpop.com>
2020-12-11 19:41:06 -0800

    Added file information to header.

--- MyCaffe.data/MnistDataLoader.cs
+++ MyCaffe.data/MnistDataLoader.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2018-2020 SignalPop LLC. All rights reserved.
+// License: Apache 2.0
+// License: https://github.com/MyCaffe/MyCaffe/blob/master/LICENSE
+// Original Source: https://github.com/MyCaffe/MyCaffe/blob/master/MyCaffe.data/MnistDataLoader.cs
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
