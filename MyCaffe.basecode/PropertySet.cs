649f0846c44edbc970dce3d110a2f5f08d3e84d9
ZoroDerVonCodier <daveb@signalpop.com>
2021-02-25 21:26:04 -0800

    Added byte array support.

--- MyCaffe.basecode/PropertySet.cs
+++ MyCaffe.basecode/PropertySet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,8 @@ namespace MyCaffe.basecode
     /// </summary>
     public class PropertySet : IEnumerable<KeyValuePair<string, string>>
     {
-        Dictionary<string, string> m_rgProperties;
+        Dictionary<string, string> m_rgProperties = new Dictionary<string, string>();
+        Dictionary<string, byte[]> m_rgBlobs = new Dictionary<string, byte[]>();
 
         /// <summary>
         /// The constructor, initialized with a dictionary of properties.
@@ -48,6 +50,65 @@ namespace MyCaffe.basecode
         }
 
         /// <summary>
+        /// The constructor.
+        /// </summary>
+        public PropertySet()
+        {
+        }
+
+        /// <summary>
+        /// Move the property from the list of properties to the list of blobs, storing the blob as a 0 terminated string of bytes.
+        /// </summary>
+        /// <param name="strName">Specifies the name of the property to move.</param>
+        /// <returns>If the property does not exist, <i>false</i> is returned, otherwise <i>true</i> is returned.</returns>
+        public bool MovePropertyToBlob(string strName)
+        {
+            if (!m_rgProperties.ContainsKey(strName))
+                return false;
+
+            string strVal = m_rgProperties[strName];
+            m_rgProperties.Remove(strName);
+
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                foreach (char ch in strVal)
+                {
+                    bw.Write((byte)ch);
+                }
+
+                bw.Write((byte)0);
+
+                m_rgBlobs.Add(strName, ms.ToArray());
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Merge a given property set into this one.
+        /// </summary>
+        /// <param name="prop">Specifies the property set to merge into this one.</param>
+        public void Merge(PropertySet prop)
+        {
+            foreach (KeyValuePair<string, string> kv in prop.m_rgProperties)
+            {
+                if (!m_rgProperties.ContainsKey(kv.Key))
+                    m_rgProperties.Add(kv.Key, kv.Value);
+                else
+                    m_rgProperties[kv.Key] = kv.Value;
+            }
+
+            foreach (KeyValuePair<string, byte[]> kv in prop.m_rgBlobs)
+            {
+                if (!m_rgBlobs.ContainsKey(kv.Key))
+                    m_rgBlobs.Add(kv.Key, kv.Value);
+                else
+                    m_rgBlobs[kv.Key] = kv.Value;
+            }
+        }
+
+        /// <summary>
         /// Returns a property as a string value.
         /// </summary>
         /// <param name="strName">Specifies the name of the property to retrieve.</param>
@@ -67,6 +128,25 @@ namespace MyCaffe.basecode
         }
 
         /// <summary>
+        /// Returns a property blob as a byte array value.
+        /// </summary>
+        /// <param name="strName">Specifies the name of the property blob to retrieve.</param>
+        /// <param name="bThrowExceptions">When <i>true</i> (the default), an exception is thrown if the property blob is not found, otherwise a value of <i>null</i> is returned when not found.</param>
+        /// <returns>The property blob value is returned.</returns>
+        public byte[] GetPropertyBlob(string strName, bool bThrowExceptions = true)
+        {
+            if (!m_rgBlobs.ContainsKey(strName))
+            {
+                if (bThrowExceptions)
+                    throw new Exception("The property '" + strName + "' was not found!");
+
+                return null;
+            }
+
+            return m_rgBlobs[strName];
+        }
+
+        /// <summary>
         /// Sets a property in the property set to a value if it exists, otherwise it adds the new property.
         /// </summary>
         /// <param name="strName">Specifies the property name.</param>
@@ -77,6 +157,19 @@ namespace MyCaffe.basecode
                 m_rgProperties.Add(strName, strVal);
             else
                 m_rgProperties[strName] = strVal;
+        }
+
+        /// <summary>
+        /// Sets a property in the blob set to a byte array if it exists, otherwise it adds the new blob.
+        /// </summary>
+        /// <param name="strName">Specifies the name of the blob.</param>
+        /// <param name="rg">Specifies the blob data.</param>
+        public void SetPropertyBlob(string strName, byte[] rg)
+        {
+            if (!m_rgBlobs.ContainsKey(strName))
+                m_rgBlobs.Add(strName, rg);
+            else
+                m_rgBlobs[strName] = rg;
         }
 
         /// <summary>
