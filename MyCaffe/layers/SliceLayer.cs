ff2748b1d0c362809718ffa690d94348c401b07a
ZoroDerVonCodier <daveb@signalpop.com>
2021-04-12 23:10:14 -0700

    Fixed bugs related to trailing 1's in bottom.

--- MyCaffe/layers/SliceLayer.cs
+++ MyCaffe/layers/SliceLayer.cs
@@ -71,7 +71,7 @@ namespace MyCaffe.layers
         /// <param name="colTop">Specifies the collection of top (output) Blobs.</param>
         public override void Reshape(BlobCollection<T> colBottom, BlobCollection<T> colTop)
         {
-            int nNumAxes = colBottom[0].num_axes;
+            int nNumAxes = colBottom[0].num_true_axes;
 
             if (m_param.slice_param.slice_dim > 0)
             {
