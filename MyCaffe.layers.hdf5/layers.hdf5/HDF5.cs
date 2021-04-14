91cfb7a98bc0086dbee458ef9f9c893524bcac5f
ZoroDerVonCodier <daveb@signalpop.com>
2021-04-13 21:48:41 -0700

    Added HDF5DataLayer support.

---
+++ MyCaffe.layers.hdf5/layers.hdf5/HDF5.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDF5DotNet;
+using MyCaffe.basecode;
+using MyCaffe.common;
+
+namespace MyCaffe.layers.hdf5
+{
+    public class HDF5<T> : IDisposable
+    {
+        Log m_log;
+        CudaDnn<T> m_cuda;
+        H5FileId m_file;
+        string m_strFile;
+
+        public HDF5(CudaDnn<T> cuda, Log log, string strFile)
+        {
+            m_strFile = strFile;
+            m_cuda = cuda;
+            m_log = log;
+
+            m_file = H5F.open(strFile, H5F.OpenMode.ACC_RDONLY);
+            if (m_file == null)
+                m_log.FAIL("Failed opening HDF5 file: '" + strFile + "'!");
+        }
+
+        private H5DataSetId load_nd_datasetEx(Blob<T> blob, string strDatasetName, bool bReshape, int nMinDim = 1, int nMaxDim = int.MaxValue)
+        {
+            H5DataSetId ds = null;
+
+            try
+            {
+                ds = H5D.open(m_file, strDatasetName);
+                if (ds == null)
+                    m_log.FAIL("Failed to find the dataset '" + strDatasetName + "'!");
+
+                // Verify that the number of dimensions are in the accepted range.
+                H5DataSpaceId dsSpace = H5D.getSpace(ds);
+                if (dsSpace == null)
+                    m_log.FAIL("Failed to get the dataset space!");
+
+                int nDims = H5S.getSimpleExtentNDims(dsSpace);
+                m_log.CHECK_GE(nDims, nMinDim, "The dataset dim is out of range!");
+                m_log.CHECK_LE(nDims, nMaxDim, "The dataset dim is out of range!");
+
+                long[] rgDims = H5S.getSimpleExtentDims(dsSpace);
+
+                // Verify that the data format is what we expect: float or double
+                H5DataTypeId dsType = H5D.getType(ds);
+                if (dsType == null)
+                    m_log.FAIL("Failed to get the dataset type!");
+
+                H5T.H5TClass dataClass = H5T.getClass(dsType);
+                switch (dataClass)
+                {
+                    case H5T.H5TClass.FLOAT:
+                        m_log.WriteLine("Datatype class: H5T_FLOAT");
+                        break;
+
+                    case H5T.H5TClass.INTEGER:
+                        m_log.WriteLine("Datatype class: H5T_INTEGER");
+                        break;
+
+                    default:
+                        m_log.FAIL("Unsupported datatype class: " + dataClass.ToString());
+                        break;
+                }
+
+                List<int> rgBlobDims = new List<int>();
+                for (int i = 0; i < nDims; i++)
+                {
+                    rgBlobDims.Add((int)rgDims[i]);
+                }
+
+                if (bReshape)
+                {
+                    blob.Reshape(rgBlobDims);
+                }
+                else
+                {
+                    if (!Utility.Compare<int>(rgBlobDims, blob.shape()))
+                    {
+                        string strSrcShape = Utility.ToString<int>(rgBlobDims);
+                        m_log.FAIL("Cannot load blob from  hdf5; shape mismatch.  Source shape = " + strSrcShape + ", target shape = " + blob.shape_string);
+                    }
+                }
+            }
+            catch (Exception excpt)
+            {
+                if (ds != null)
+                {
+                    H5D.close(ds);
+                    ds = null;
+                }
+
+                throw excpt;
+            }
+
+            return ds;
+        }
+
+        public void load_nd_dataset(Blob<T> blob, string strDatasetName, bool bReshape = false, int nMinDim = 1, int nMaxDim = int.MaxValue)
+        {
+            H5DataSetId ds = null;
+
+            try
+            {
+                ds = load_nd_datasetEx(blob, strDatasetName, bReshape, nMinDim, nMaxDim);
+
+                H5DataTypeId dsType = H5D.getType(ds);
+                int nSize = H5T.getSize(dsType);
+
+                if (nSize == sizeof(double))
+                {
+                    double[] rgBuffer = new double[blob.count()];
+                    H5Array<double> rgData = new H5Array<double>(rgBuffer);
+
+                    H5D.read<double>(ds, dsType, rgData);
+                    blob.mutable_cpu_data = Utility.ConvertVec<T>(rgBuffer);
+                }
+                else if (nSize == sizeof(float))
+                {
+                    float[] rgBuffer = new float[blob.count()];
+                    H5Array<float> rgData = new H5Array<float>(rgBuffer);
+
+                    H5D.read<float>(ds, dsType, rgData);
+                    blob.mutable_cpu_data = Utility.ConvertVec<T>(rgBuffer);
+                }
+                else
+                    m_log.FAIL("The dataset size of '" + nSize.ToString() + "' is not supported!");
+            }
+            catch (Exception excpt)
+            {
+            }
+            finally
+            {
+                if (ds != null)
+                    H5D.close(ds);
+            }
+        }
+      
+        public void Dispose()
+        {
+            if (m_file != null)
+            {
+                H5F.close(m_file);
+                m_file = null;
+            }
+        }
+    }
+}
---
+++ MyCaffe.layers.hdf5/layers.hdf5/HDF5DataLayer.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyCaffe.basecode;
+using MyCaffe.common;
+using MyCaffe.param;
+using MyCaffe.fillers;
+using System.IO;
+
+namespace MyCaffe.layers.hdf5
+{
+    /// <summary>
+    /// The HDF5DataLayer loads data from files in the HDF5 data format.
+    /// This layer is initialized with the MyCaffe.param.HDF5DataParameter.
+    /// </summary>
+    /// <typeparam name="T">Specifies the base type <i>float</i> or <i>double</i>.  Using <i>float</i> is recommended to conserve GPU memory.</typeparam>
+    public class HDF5DataLayer<T> : Layer<T>
+    {
+        List<string> m_rgstrFileNames = new List<string>();
+        int m_nNumFiles = 0;
+        int m_nCurrentFile = 0;
+        int m_nCurrentRow = 0;
+        BlobCollection<T> m_colHdfBlobs = new BlobCollection<T>();
+        List<int> m_rgDataPermutation = new List<int>();
+        List<int> m_rgFilePermutation = new List<int>();
+        ulong m_lOffset = 0;
+
+        /// <summary>
+        /// The HDF5DataLayer constructor.
+        /// </summary>
+        /// <param name="cuda">Specifies the CudaDnn connection to Cuda.</param>
+        /// <param name="log">Specifies the Log for output.</param>
+        /// <param name="p">
+        /// Provides DummyDataParameter hdf5_data_param with options:
+        ///  - data_filler. A list of Fillers to use.
+        ///  
+        ///  - shape.  A list of shapes to use.
+        /// </param>
+        public HDF5DataLayer(CudaDnn<T> cuda, Log log, LayerParameter p)
+            : base(cuda, log, p)
+        {
+            m_type = LayerParameter.LayerType.HDF5_DATA;
+        }
+       
+        protected virtual void LoadHDF5FileData(string strFile)
+        {
+            m_log.WriteLine("Loading HDF5 file: '" + strFile + "'");
+            HDF5<T> hdf5 = new HDF5<T>(m_cuda, m_log, strFile);
+
+            int nTopCount = m_param.top.Count;
+            for (int i = 0; i < nTopCount; i++)
+            {
+                // Allow reshape here, as we are loading data not params.
+                Blob<T> blob = new Blob<T>(m_cuda, m_log, false);
+                hdf5.load_nd_dataset(blob, m_param.top[i], true);
+
+                m_colBlobs.Add(blob);
+            }
+
+            hdf5.Dispose();
+
+            // MinTopBlobs=1 guarantees at least one top blob
+            m_log.CHECK_GE(m_colBlobs[0].num_axes, 1, "Input must have at least 1 axis.");
+            int nNum = m_colBlobs[0].shape(0);
+
+            for (int i = 1; i < nTopCount; i++)
+            {
+                m_log.CHECK_EQ(m_colBlobs[i].shape(0), nNum, "The 'num' on all blobs must be equal.");
+            }
+
+            // Default to identity permutation.
+            m_rgDataPermutation = new List<int>();
+            for (int i = 0; i < nNum; i++)
+            {
+                m_rgDataPermutation.Add(i);
+            }
+
+            // Shuffle if needed
+            if (m_param.hdf5_data_param.shuffle)
+            {
+                m_rgDataPermutation = Utility.RandomShuffle(m_rgDataPermutation);
+                m_log.WriteLine("Successfully loaded " + nNum.ToString() + " rows (shuffled).");
+            }
+            else
+            {
+                m_log.WriteLine("Successfully loaded " + nNum.ToString() + " rows.");
+            }
+        }
+
+        /// <summary>
+        /// Returns 0 for data layers have no bottom (input) Blobs.
+        /// </summary>
+        public override int ExactNumBottomBlobs
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Returns the minimum number of required top (output) Blobs: data
+        /// </summary>
+        public override int MinTopBlobs
+        {
+            get { return 1; }
+        }
+
+        /// <summary>
+        /// Setup the layer.
+        /// </summary>
+        /// <param name="colBottom">Not used.</param>
+        /// <param name="colTop">Specifies the collection of top (output) Blobs.</param>
+        public override void LayerSetUp(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+            // Refuse transformation parameters since HDF5 is totally generic.
+            if (m_param.transform_param != null)
+                m_log.WriteLine("WARNING: " + m_type.ToString() + " does not transform data.");
+
+            // Read the source to parse the filenames.
+            m_log.WriteLine("Loading list of HDF5 file names from: " + m_param.hdf5_data_param.source);
+            m_rgstrFileNames = Utility.LoadTextLines(m_param.hdf5_data_param.source, m_log);
+            m_nNumFiles = m_rgstrFileNames.Count;
+            m_nCurrentFile = 0;
+
+            m_log.WriteLine("Number of HDF5 files: " + m_nNumFiles.ToString());
+            m_log.CHECK_GE(m_nNumFiles, 1, "Must have at least one HDF5 filename listed in '" + m_param.hdf5_data_param.source + "'!");
+
+            // Default to identity permutation.
+            m_rgFilePermutation = new List<int>();
+            for (int i = 0; i < m_nNumFiles; i++)
+            {
+                m_rgFilePermutation.Add(i);
+            }
+
+            // Shuffle if needed.
+            if (m_param.hdf5_data_param.shuffle)
+                m_rgFilePermutation = Utility.RandomShuffle(m_rgFilePermutation);
+
+            // Load the first HDF5 file and initialize the line counter.
+            LoadHDF5FileData(m_rgstrFileNames[m_rgFilePermutation[m_nCurrentFile]]);
+            m_nCurrentRow = 0;
+
+            // Reshape the blobs.
+            int nBatchSize = (int)m_param.hdf5_data_param.batch_size;
+            int nTopSize = m_param.top.Count;
+            List<int> rgTopShape = new List<int>();
+
+            for (int i = 0; i < nTopSize; i++)
+            {
+                rgTopShape = Utility.Clone<int>(m_colBlobs[i].shape());
+                rgTopShape[0] = nBatchSize;
+                colTop[i].Reshape(rgTopShape);
+            }
+        }
+
+        protected bool Skip()
+        {
+            ulong nSize = (ulong)m_param.solver_count;
+            ulong nRank = (ulong)m_param.solver_rank;
+            // In test mode, only rank 0 runs, so avoid skipping.
+            bool bKeep = (m_lOffset % nSize) == nRank || m_param.phase == Phase.TEST;
+
+            return !bKeep;
+        }
+
+        protected void Next()
+        {
+            m_nCurrentRow++;
+
+            if (m_nCurrentRow == m_colBlobs[0].shape(0))
+            {
+                if (m_nNumFiles > 1)
+                {
+                    m_nCurrentFile++;
+
+                    if (m_nCurrentFile == m_nNumFiles)
+                    {
+                        m_nCurrentFile = 0;
+                        if (m_param.hdf5_data_param.shuffle)
+                            m_rgFilePermutation = Utility.RandomShuffle(m_rgFilePermutation);
+                        m_log.WriteLine("Looping around to first file...");
+                    }
+
+                    LoadHDF5FileData(m_rgstrFileNames[m_rgFilePermutation[m_nCurrentFile]]);
+                }
+
+                m_nCurrentRow = 0;
+
+                if (m_param.hdf5_data_param.shuffle)
+                    m_rgDataPermutation = Utility.RandomShuffle(m_rgDataPermutation);
+            }
+
+            m_lOffset++;
+        }
+
+        /// <summary>
+        /// Data layers have no bottoms, so reshaping is trivial.
+        /// </summary>
+        /// <param name="colBottom">Not used.</param>
+        /// <param name="colTop">Specifies the collection of top (output) Blobs.</param>
+        public override void Reshape(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+        }
+
+        /// <summary>
+        /// Run the Forward computation, which fills the data into the top (output) Blobs.
+        /// </summary>
+        /// <param name="colBottom">Not used.</param>
+        /// <param name="colTop">top output blob vector (length 1)
+        ///  -# @f$ (N \times C \times H \times W) @f$
+        ///     the data outputs.  
+        /// </param>
+        protected override void forward(BlobCollection<T> colBottom, BlobCollection<T> colTop)
+        {
+            int nBatch = (int)m_param.hdf5_data_param.batch_size;
+
+            for (int i = 0; i < nBatch; i++)
+            {
+                while (Skip())
+                    Next();
+
+                for (int j = 0; j < m_param.top.Count; j++)
+                {
+                    int nDataDim = colTop[j].count() / colTop[j].shape(0);
+                    int nSrcIdx = m_rgDataPermutation[m_nCurrentRow];
+                    int nDstIdx = i * nDataDim;
+                    m_cuda.copy(nDataDim, m_colBlobs[j].gpu_data, colTop[j].mutable_gpu_data, nSrcIdx, nDstIdx);
+                }
+
+                Next();
+            }
+        }
+
+        /// @brief Not implemented - data Layers do not perform backward..
+        protected override void backward(BlobCollection<T> colTop, List<bool> rgbPropagateDown, BlobCollection<T> colBottom)
+        {
+        }
+    }
+}
