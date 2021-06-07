268ed4a0502748f05fef7acb601f795231976236
ZoroDerVonCodier <daveb@signalpop.com>
2021-06-07 07:30:49 -0700

    Fixed bug where search now re-run on full branch to maintain correct state.

--- MyCaffe/common/BeamSearch.cs
+++ MyCaffe/common/BeamSearch.cs
@@ -46,19 +46,17 @@ namespace MyCaffe.common
         /// </summary>
         /// <param name="input">Specifies the input data (e.g. the encoder input)</param>
         /// <param name="nK">Specifies the beam width for the search.</param>
-        /// <param name="dfThreshold">Specifies the threshold where detected items with probabilities less than the threshold are ignored (default = 0.2).</param>
+        /// <param name="dfThreshold">Specifies the threshold where detected items with probabilities less than the threshold are ignored (default = 0.01).</param>
         /// <param name="nMax">Specifies the maximum length to process (default = 80)</param>
         /// <returns>The list of top sequences is returned.</returns>
         /// <remarks>
         /// The beam-search algorithm is inspired by the article
         /// @see [How to Implement a Beam Search Decoder for Natural Language Processing](https://machinelearningmastery.com/beam-search-decoder-natural-language-processing/) by Jason Brownlee, "Machine Learning Mastery", 2018
         /// </remarks>
-        public List<Tuple<double, List<Tuple<string, int, double>>>> Search(PropertySet input, int nK, double dfThreshold = 0.2, int nMax = 80)
+        public List<Tuple<double, bool, List<Tuple<string, int, double>>>> Search(PropertySet input, int nK, double dfThreshold = 0.01, int nMax = 80)
         {
-            List<Tuple<double, List<Tuple<string, int, double>>>> rgSequences = new List<Tuple<double, List<Tuple<string, int, double>>>>();
-            rgSequences.Add(new Tuple<double, List<Tuple<string, int, double>>>(0, new List<Tuple<string, int, double>>()));
-
-            List<Tuple<double, List<Tuple<string, int, double>>>> rgAllCandidates = new List<Tuple<double, List<Tuple<string, int, double>>>>();
+            List<Tuple<double, bool, List<Tuple<string, int, double>>>> rgSequences = new List<Tuple<double, bool, List<Tuple<string, int, double>>>>();
+            rgSequences.Add(new Tuple<double, bool, List<Tuple<string, int, double>>>(0, false, new List<Tuple<string, int, double>>()));
 
             BlobCollection<T> colBottom = m_layer.PreProcessInput(input, null);
             double dfLoss;
@@ -76,7 +74,7 @@ namespace MyCaffe.common
             {
                 int nProcessedCount = 0;
 
-                List<Tuple<double, List<Tuple<string, int, double>>>> rgCandidates = new List<Tuple<double, List<Tuple<string, int, double>>>>();
+                List<Tuple<double, bool, List<Tuple<string, int, double>>>> rgCandidates = new List<Tuple<double, bool, List<Tuple<string, int, double>>>>();
 
                 for (int i = 0; i < rgSequences.Count; i++)
                 {
@@ -89,17 +87,17 @@ namespace MyCaffe.common
                                 double dfScore = rgSequences[i].Item1 - Math.Log(rgrgRes[i][j].Item3);
 
                                 List<Tuple<string, int, double>> rgSequence1 = new List<Tuple<string, int, double>>();
-                                rgSequence1.AddRange(rgSequences[i].Item2);
+                                rgSequence1.AddRange(rgSequences[i].Item3);
                                 rgSequence1.Add(rgrgRes[i][j]);
 
-                                rgCandidates.Add(new Tuple<double, List<Tuple<string, int, double>>>(dfScore, rgSequence1));
+                                rgCandidates.Add(new Tuple<double, bool, List<Tuple<string, int, double>>>(dfScore, false, rgSequence1));
                                 nProcessedCount++;
                             }
                         }
                     }
                     else
                     {
-                        rgCandidates.Add(rgSequences[i]);
+                        rgCandidates.Add(new Tuple<double, bool, List<Tuple<string, int, double>>>(rgSequences[i].Item1, true, rgSequences[i].Item3));
                     }
                 }
 
@@ -107,33 +105,45 @@ namespace MyCaffe.common
                 {
                     rgSequences = rgCandidates.OrderBy(p => p.Item1).Take(nK).ToList();
                     rgrgRes = new List<List<Tuple<string, int, double>>>();
-                    Dictionary<int, List<Tuple<string, int, double>>> rgProcessed = new Dictionary<int, List<Tuple<string, int, double>>>();
 
                     for (int i = 0; i < rgSequences.Count; i++)
                     {
-                        rgRes = new List<Tuple<string, int, double>>();
-                        int nIdx = rgSequences[i].Item2.Last().Item2;
-
-                        if (!rgProcessed.ContainsKey(nIdx))
+                        if (!rgSequences[i].Item2)
                         {
-                            m_layer.PreProcessInput(strInput, nIdx, colBottom);
-                            colTop = m_net.Forward(colBottom, out dfLoss, true);
-                            List<Tuple<string, int, double>> rgRes1 = m_layer.PostProcessOutput(colTop[0], nK);
-                            rgRes1 = rgRes1.Where(p => p.Item3 >= dfThreshold).ToList();
+                            rgRes = new List<Tuple<string, int, double>>();
 
-                            for (int j = 0; j < rgRes1.Count; j++)
+                            // Reset state.
+                            m_layer.PreProcessInput(strInput, 1, colBottom);
+                            m_net.Forward(colBottom, out dfLoss, true);
+
+                            // Re-run through each branch to get correct state at the leaf
+                            for (int j = 0; j < rgSequences[i].Item3.Count; j++)
                             {
-                                if (rgRes1[j].Item1.Length > 0)
-                                    rgRes.Add(rgRes1[j]);
-                                else
-                                    Trace.WriteLine("EOS");
-                            }
+                                int nIdx = rgSequences[i].Item3[j].Item2;
 
-                            rgrgRes.Add(rgRes);
+                                m_layer.PreProcessInput(strInput, nIdx, colBottom);
+                                colTop = m_net.Forward(colBottom, out dfLoss, true);
+
+                                if (j == rgSequences[i].Item3.Count - 1)
+                                {
+                                    List<Tuple<string, int, double>> rgRes1 = m_layer.PostProcessOutput(colTop[0], nK);
+                                    rgRes1 = rgRes1.Where(p => p.Item3 >= dfThreshold).ToList();
+
+                                    for (int k = 0; k < rgRes1.Count; k++)
+                                    {
+                                        if (rgRes1[k].Item1.Length > 0)
+                                            rgRes.Add(rgRes1[k]);
+                                        else
+                                            Trace.WriteLine("EOS");
+                                    }
+
+                                    rgrgRes.Add(rgRes);
+                                }
+                            }
                         }
                         else
                         {
-                            rgrgRes.Add(rgProcessed[nIdx]);
+                            rgrgRes.Add(new List<Tuple<string, int, double>>());
                         }
                     }
                 }
