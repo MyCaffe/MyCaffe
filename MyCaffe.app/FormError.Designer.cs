c9d5e0c11bc7b790757a21d3a9e11db19e28ed10
ZoroDerVonCodier <daveb@signalpop.com>
2021-11-06 21:43:34 -0700

    Added error catch all dialog.

---
+++ MyCaffe.app/FormError.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MyCaffe.app
+{
+    partial class FormError
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormError));
+            this.label1 = new System.Windows.Forms.Label();
+            this.edtError = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtLocation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Error:";
+            // 
+            // edtError
+            // 
+            this.edtError.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edtError.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edtError.Location = new System.Drawing.Point(15, 25);
+            this.edtError.Multiline = true;
+            this.edtError.Name = "edtError";
+            this.edtError.Size = new System.Drawing.Size(931, 283);
+            this.edtError.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Location:";
+            // 
+            // edtLocation
+            // 
+            this.edtLocation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edtLocation.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edtLocation.Location = new System.Drawing.Point(15, 327);
+            this.edtLocation.Multiline = true;
+            this.edtLocation.Name = "edtLocation";
+            this.edtLocation.Size = new System.Drawing.Size(931, 272);
+            this.edtLocation.TabIndex = 1;
+            // 
+            // FormError
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 611);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.edtLocation);
+            this.Controls.Add(this.edtError);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormError";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MyCaffe Application Error";
+            this.Load += new System.EventHandler(this.FormError_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edtError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edtLocation;
+    }
+}
---
+++ MyCaffe.app/FormError.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCaffe.app
+{
+    public partial class FormError : Form
+    {
+        string m_strType;
+        Exception m_err;
+
+        public FormError(string strErrorType, Exception err)
+        {
+            m_strType = strErrorType;
+            m_err = err;
+            InitializeComponent();
+        }
+
+        private void FormError_Load(object sender, EventArgs e)
+        {
+            Text += " - " + m_strType;
+
+            Exception err = m_err;
+
+            while (err != null)
+            {
+                edtError.Text += err.Message;
+                edtError.Text += Environment.NewLine;
+                err = err.InnerException;
+            }
+
+            edtLocation.Text = err.StackTrace;
+        }
+    }
+}
