23e5e2b07e1bade25c7358c4a122c2c489175227
ZoroDerVonCodier <daveb@signalpop.com>
2021-12-11 19:27:13 -0800

    Added driver version to verbose GetDriverInfo

--- CudaDnnDLL/Cuda Files/device.cu
+++ CudaDnnDLL/Cuda Files/device.cu
@@ -449,8 +449,15 @@ long Device<T>::GetDeviceInfo(int nDevice, LPTSTR* pszDevice, bool bVerbose)
 		m_nMajor = prop.major;
 		m_nMinor = prop.minor;
 
+		float fDriverVer = 0;
+		NvU32 v;
+		NvAPI_ShortString bVer;
+		status = NvAPI_SYS_GetDriverAndBranchVersion(&v, bVer);
+		if (status == NVAPI_OK)
+			fDriverVer = (float)v / 100.0f;
+
 		char szBuffer[1024];
-		_snprintf(szBuffer, 1023, "\r\n Major: %d, Minor: %d, Compute Mode: %d\r\n Max Grid: { %d, %d, %d }, Max Thread Dim: { %d, %d, %d }\r\n Shared Memory/Block: %zd\r\n", prop.major, prop.minor, prop.computeMode, prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2], prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2], prop.sharedMemPerBlock);
+		_snprintf(szBuffer, 1023, "\r\n Major: %d, Minor: %d, Compute Mode: %d\r\n Max Grid: { %d, %d, %d }, Max Thread Dim: { %d, %d, %d }\r\n Shared Memory/Block: %zd\r\n Driver Version: %.2f", prop.major, prop.minor, prop.computeMode, prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2], prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2], prop.sharedMemPerBlock, fDriverVer);
 		_tcsncat(pDst, A2T(szBuffer), 2047);
 	}
 
