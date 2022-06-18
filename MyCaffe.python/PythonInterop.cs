665f703897429d749e74ff1bbeda7019052e75a1
Dave Brown <daveb@signalpop.com>
2022-06-18 14:31:17 -0700

    Added comments.

--- MyCaffe.python/PythonInterop.cs
+++ MyCaffe.python/PythonInterop.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// The MyCaffe.python namespace contains python related classes to help improve interop between C# and Python.  
+/// </summary>
 namespace MyCaffe.python
 {
     /// <summary>
@@ -15,17 +18,35 @@ namespace MyCaffe.python
     /// </remarks>
     public class PythonInterop
     {
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="strPythonDllPath">Specifies the path to the Python DLL (e.g. Python39.dll) which is usually located
+        /// at: @"C:\Users\" + strUserName + @"\AppData\Local\Programs\Python\Python39\python39.dll"</param>
+        /// <remarks>
+        /// See https://github.com/MyCaffe/MyCaffe/blob/master/MyCaffe.test/TestPython.cs for an example that demonstrates how
+        /// to use the PythonInterop class to run the GPT2 pre-trained model from HuggingFace to generate text.
+        /// </remarks>
         public PythonInterop(string strPythonDllPath)
         {
             Initialize(strPythonDllPath);
         }
 
+        /// <summary>
+        /// Initialize the Python Engine with the version of Python used.
+        /// </summary>
+        /// <param name="strPythonDllPath">Specifies the path to the Python DLL (e.g. Python39.dll) which is usually located
+        /// at: @"C:\Users\" + strUserName + @"\AppData\Local\Programs\Python\Python39\python39.dll"</param>
         public void Initialize(string strPythonDllPath)
         {
             Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", strPythonDllPath);
             PythonEngine.Initialize();
         }
 
+        /// <summary>
+        /// Run the Python code specified.
+        /// </summary>
+        /// <param name="pycode">Specifies a block of Python code to run.</param>
         public void RunPythonCode(string pycode)
         {
             using (Py.GIL())
@@ -34,6 +55,12 @@ namespace MyCaffe.python
             }
         }
 
+        /// <summary>
+        /// Run the Python code specified, passing a parameter to it from C#
+        /// </summary>
+        /// <param name="pycode">Specifies a block of Python code to run.</param>
+        /// <param name="parameter">Specifies the parameter value.</param>
+        /// <param name="parameterName">Specifies the parameter name within the Python code.</param>
         public void RunPythonCode(string pycode, object parameter, string parameterName)
         {
             using (Py.GIL())
@@ -47,6 +74,14 @@ namespace MyCaffe.python
             }
         }
 
+        /// <summary>
+        /// Run the Python code specified, passing a parameter to it from C# and receiving a return value
+        /// </summary>
+        /// <param name="pycode">Specifies a block of Python code to run.</param>
+        /// <param name="parameter">Specifies the parameter value.</param>
+        /// <param name="parameterName">Specifies the parameter name within the Python code.</param>
+        /// <param name="returnedVariableName">Specifies the name of the return value in the Python code.</param>
+        /// <returns>The Python return value is returned.</returns>
         public object RunPythonCodeAndReturn(string pycode, object parameter, string parameterName, string returnedVariableName)
         {
             object returnedVariable = new object();
@@ -62,6 +97,13 @@ namespace MyCaffe.python
             return returnedVariable;
         }
 
+        /// <summary>
+        /// Run the Python code specified, passing a set of parameters to it from C# and receiving a return value
+        /// </summary>
+        /// <param name="pycode">Specifies a block of Python code to run.</param>
+        /// <param name="returnedVariableName">Specifies the name of the return value in the Python code.</param>
+        /// <param name="rgArg">Specifies a list of parameter name, value pairs.</param>
+        /// <returns>The Python return value is returned.</returns>
         public object RunPythonCodeAndReturn(string pycode, string returnedVariableName, params KeyValuePair<string, object>[] rgArg)
         {
             object returnedVariable = new object();
