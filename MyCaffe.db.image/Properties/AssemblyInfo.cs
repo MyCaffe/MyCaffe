2fb0b5174bc59c31a557ef7436d414c308a3abfa
Dave Brown <daveb@signalpop.com>
2022-11-23 13:08:51 -0800

    Release 1.11.8.27 ready.

--- MyCaffe.db.image/Properties/AssemblyInfo.cs
+++ MyCaffe.db.image/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.7.7")]
-[assembly: AssemblyFileVersion("1.11.7.7")]
+[assembly: AssemblyVersion("1.11.8.27")]
+[assembly: AssemblyFileVersion("1.11.8.27")]
