0c6735e18c31837a41d96e31b660847215c95cb4
Dave Brown <daveb@signalpop.com>
2022-12-26 16:57:48 -0800

    Added test data from Python test project.

--- MyCaffe.test/TestMultiheadAttentionLayer.cs
+++ MyCaffe.test/TestMultiheadAttentionLayer.cs
@@ -11,6 +11,9 @@ using MyCaffe.layers;
 using System.Diagnostics;
 using System.IO;
 using MyCaffe.param.gpt;
+using System.Net;
+using System.Threading;
+using System.IO.Compression;
 
 namespace MyCaffe.test
 {
@@ -26,7 +29,7 @@ namespace MyCaffe.test
             {
                 foreach (IMultiheadAttentionLayerTest t in test.Tests)
                 {
-                    t.TestForward(false, 1);
+                    t.TestForward(3, 8);
                 }
             }
             finally
@@ -44,7 +47,7 @@ namespace MyCaffe.test
             {
                 foreach (IMultiheadAttentionLayerTest t in test.Tests)
                 {
-                    t.TestBackward(false, 1);
+                    t.TestBackward(3, 8);
                 }
             }
             finally
@@ -62,7 +65,7 @@ namespace MyCaffe.test
             {
                 foreach (IMultiheadAttentionLayerTest t in test.Tests)
                 {
-                    t.TestGradient(false, 1);
+                    t.TestGradient(3, 8);
                 }
             }
             finally
@@ -74,9 +77,9 @@ namespace MyCaffe.test
 
     interface IMultiheadAttentionLayerTest : ITest
     {
-        void TestForward(bool bBatch, int nHeads);
-        void TestBackward(bool bBatch, int nHeads);
-        void TestGradient(bool bBatch, int nHeads);
+        void TestForward(int nBatch, int nHeads);
+        void TestBackward(int nBatch, int nHeads);
+        void TestGradient(int nBatch, int nHeads);
     }
 
     class MultiheadAttentionLayerTest : TestBase
@@ -98,12 +101,27 @@ namespace MyCaffe.test
     class MultiheadAttentionLayerTest2<T> : TestEx<T>, IMultiheadAttentionLayerTest
     {
         Blob<T> m_blobY;
+        Blob<T> m_blobQ;
+        Blob<T> m_blobK;
+        Blob<T> m_blobV;
+        Blob<T> m_blobQexp;
+        Blob<T> m_blobKexp;
+        Blob<T> m_blobVexp;
+        Stopwatch m_swUpdateTimer = new Stopwatch();
+        double m_dfLastProgress = 0;
+        AutoResetEvent m_evtDownloadDone = new AutoResetEvent(false);
 
         public MultiheadAttentionLayerTest2(string strName, int nDeviceID, EngineParameter.Engine engine)
             : base(strName, new List<int>() { 3, 2, 4, 1 }, nDeviceID)
         {
             m_engine = engine;
+            m_blobQ = new Blob<T>(m_cuda, m_log);
+            m_blobK = new Blob<T>(m_cuda, m_log);
+            m_blobV = new Blob<T>(m_cuda, m_log);
             m_blobY = new Blob<T>(m_cuda, m_log);
+            m_blobQexp = new Blob<T>(m_cuda, m_log);
+            m_blobKexp = new Blob<T>(m_cuda, m_log);
+            m_blobVexp = new Blob<T>(m_cuda, m_log);
         }
 
         protected override FillerParameter getFillerParam()
@@ -122,106 +140,128 @@ namespace MyCaffe.test
 
         protected override void dispose()
         {
+            dispose(ref m_blobQ);
+            dispose(ref m_blobK);
+            dispose(ref m_blobV);
+            dispose(ref m_blobQexp);
+            dispose(ref m_blobKexp);
+            dispose(ref m_blobVexp);
             dispose(ref m_blobY);
             base.dispose();
         }
 
-        public Tuple<List<int>, float[]> Fill(string strGpt, string strName, Log log, string strPass = "")
+        private string loadTestData()
         {
-            string strFile = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\MyCaffe\\test_data\\data\\text\\gpt\\" + strGpt + "\\";
+            string strTestDataFile = downloadTestData();
+            string strPath = Path.GetDirectoryName(strTestDataFile);
+            
+            if (!File.Exists(strPath + "\\test\\10_concat_output1.npy"))
+                ZipFile.ExtractToDirectory(strTestDataFile, strPath);
 
-            if (!string.IsNullOrEmpty(strPass))
-                strFile += strPass + "\\";
+            return strPath + "\\test\\";
+        }
 
-            strFile += strName + ".txt";
+        private string downloadTestData()
+        {
+            string strTestDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\MyCaffe\\test_data\\";
+            if (!Directory.Exists(strTestDataPath))
+                Directory.CreateDirectory(strTestDataPath);
 
-            string[] rgstrLines = File.ReadAllLines(strFile);
-            string strSize = rgstrLines[0].Trim('#', ' ', '(', ')', ',');
-            string[] rgstrSize = strSize.Split(',');
-            List<int> rgnShape = new List<int>() { 1 };
-
-            if (!string.IsNullOrEmpty(strSize))
-                rgnShape = rgstrSize.Select(p1 => int.Parse(p1)).ToList();
-            List<float> rgfVal = new List<float>();
-
-            while (rgnShape.Count < 4)
+            string strTestDataFile = strTestDataPath + "_multihead_test.zip";
+            if (!File.Exists(strTestDataFile))
             {
-                rgnShape.Add(1);
-            }
-
-            int nCount = 1;
-            foreach (int nDim in rgnShape)
-            {
-                nCount *= nDim;
-            }
-
-            for (int i = 1; i < rgstrLines.Length; i++)
-            {
-                string[] rgstrVals = rgstrLines[i].Split(' ');
-
-                for (int j = 0; j < rgstrVals.Length; j++)
+                using (WebClient webClient = new WebClient())
                 {
-                    string strVal = rgstrVals[j].Trim();
+                    string strUrl = "https://signalpopcdn.blob.core.windows.net/mycaffesupport/_multihead_test.zip";
+                    string strFile1 = "_multihead_test.zip";
+                    string strFile = strTestDataPath + strFile1;
 
-                    if (!string.IsNullOrEmpty(strVal))
-                    {
-                        float fVal = float.Parse(strVal);
-                        rgfVal.Add(fVal);
-                    }
+                    m_swUpdateTimer.Start();
+                    m_dfLastProgress = 0;
+
+                    webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+                    webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+                    webClient.DownloadFileAsync(new Uri(strUrl), strFile, strFile1);
+
+                    m_evtDownloadDone.WaitOne();
                 }
             }
 
-            log.CHECK_EQ(rgfVal.Count, nCount, "The bottom count does not match the number of values read in!");
+            return strTestDataFile;
+        }
 
-            float[] rgf = rgfVal.ToArray();
+        private void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            bool bTraceEnabled = m_log.EnableTrace;
+            m_log.EnableTrace = true;
+            m_log.WriteLine("Downloading done.");
+            m_log.EnableTrace = bTraceEnabled;
 
-            return new Tuple<List<int>, float[]>(rgnShape, rgf);
+            m_evtDownloadDone.Set();
+        }
+
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (m_swUpdateTimer.Elapsed.TotalMilliseconds >= 1000)
+            {
+                if (m_dfLastProgress != e.ProgressPercentage)
+                {
+                    m_dfLastProgress = e.ProgressPercentage;
+                    string strFile = e.UserState.ToString();
+                    bool bTraceEnabled = m_log.EnableTrace;
+                    m_log.EnableTrace = true;
+
+                    m_log.Progress = e.ProgressPercentage / 100.0;
+                    m_log.WriteLine("Downloading '" + strFile + "' at " + m_log.Progress.ToString("P") + "...");
+                    m_log.EnableTrace = bTraceEnabled;
+                }
+
+                m_swUpdateTimer.Restart();
+            }
         }
 
         /// <summary>
         /// WORK IN PROGRESS
         /// </summary>
-        public void TestForward(bool bBatch, int nHeads)
+        public void TestForward(int nBatch, int nHeads)
         {
+            string strTestDataPath = loadTestData();
+
             LayerParameter p = new LayerParameter(LayerParameter.LayerType.MULTIHEAD_ATTENTION);
             p.multihead_attention_param.heads = nHeads;
-            p.multihead_attention_param.embed = 3;
-            p.multihead_attention_param.block_size = 4;
+            p.multihead_attention_param.embed = 512;
+            p.multihead_attention_param.block_size = 200;
             p.multihead_attention_param.attn_dropout = 0.0;
             p.multihead_attention_param.resid_dropout = 0.0;
             Layer<T> layer = Layer<T>.Create(m_cuda, m_log, p, new CancelEvent());
 
             try
             {
-                string strModel = "gpt-pico-csa";
-                if (nHeads > 1)
-                    strModel += nHeads.ToString();
-                if (bBatch)
-                    strModel += "B";
-
                 m_log.CHECK(layer.type == LayerParameter.LayerType.MULTIHEAD_ATTENTION, "The layer type is incorrect!");
 
-                Tuple<List<int>, float[]> x = Fill(strModel, "x", m_log);
-                m_blob_bottom.Reshape(x.Item1);
-                m_blob_bottom.mutable_cpu_data = convert(x.Item2);
-                
-                Tuple<List<int>, float[]> y = Fill(strModel, "y", m_log);
-                m_blobY.Reshape(y.Item1);
-                m_blobY.mutable_cpu_data = convert(y.Item2);
-                
-                Tuple<List<int>, float[]> attnBias = Fill(strModel, "attn_bias", m_log);
-                Tuple<List<int>, float[]> attnWt = Fill(strModel, "attn_weight", m_log);
-                Tuple<List<int>, float[]> projBias = Fill(strModel, "attn_proj_bias", m_log);
-                Tuple<List<int>, float[]> projWt = Fill(strModel, "attn_proj_weight", m_log);
+                m_blobQ.LoadFromNumpy(strTestDataPath + "q0.npy");
+                m_blobK.LoadFromNumpy(strTestDataPath + "k0.npy");
+                m_blobV.LoadFromNumpy(strTestDataPath + "v0.npy");
+
+                BottomVec.Clear();
+                BottomVec.Add(m_blobQ);
+                BottomVec.Add(m_blobK);
+                BottomVec.Add(m_blobV);
 
                 layer.Setup(BottomVec, TopVec);
 
-                layer.blobs[0].mutable_cpu_data = convert(attnWt.Item2);
-                layer.blobs[1].mutable_cpu_data = convert(attnBias.Item2);
-                layer.blobs[2].mutable_cpu_data = convert(projWt.Item2);
-                layer.blobs[3].mutable_cpu_data = convert(projBias.Item2);
+                layer.blobs[0].LoadFromNumpy(strTestDataPath + "w_q_weight.npy");
+                layer.blobs[1].LoadFromNumpy(strTestDataPath + "w_q_bias.npy");
+                layer.blobs[2].LoadFromNumpy(strTestDataPath + "w_k_weight.npy");
+                layer.blobs[3].LoadFromNumpy(strTestDataPath + "w_k_bias.npy");
+                layer.blobs[4].LoadFromNumpy(strTestDataPath + "w_v_weight.npy");
+                layer.blobs[5].LoadFromNumpy(strTestDataPath + "w_v_bias.npy");
+                layer.blobs[6].LoadFromNumpy(strTestDataPath + "w_o_weight.npy");
+                layer.blobs[7].LoadFromNumpy(strTestDataPath + "w_o_bias.npy");
 
                 layer.Forward(BottomVec, TopVec);
+
+                m_blobY.LoadFromNumpy(strTestDataPath + "12_output.npy");
 
                 // Now, check values
                 float[] rgExpected = convertF(m_blobY.mutable_cpu_data);
@@ -245,62 +285,57 @@ namespace MyCaffe.test
         /// <summary>
         /// WORK IN PROGRESS
         /// </summary>
-        public void TestBackward(bool bBatch, int nHeads)
+        public void TestBackward(int nBatch, int nHeads)
         {
+            string strTestDataPath = loadTestData();
+
             LayerParameter p = new LayerParameter(LayerParameter.LayerType.MULTIHEAD_ATTENTION);
             p.multihead_attention_param.heads = nHeads;
-            p.multihead_attention_param.embed = 3;
-            p.multihead_attention_param.block_size = 4;
+            p.multihead_attention_param.embed = 512;
+            p.multihead_attention_param.block_size = 200;
             p.multihead_attention_param.attn_dropout = 0.0;
             p.multihead_attention_param.resid_dropout = 0.0;
             Layer<T> layer = Layer<T>.Create(m_cuda, m_log, p, new CancelEvent());
 
             try
             {
-                string strModel = "gpt-pico-csa";
-                if (nHeads > 1)
-                    strModel += nHeads.ToString();
-                if (bBatch)
-                    strModel += "B";
-
                 m_log.CHECK(layer.type == LayerParameter.LayerType.MULTIHEAD_ATTENTION, "The layer type is incorrect!");
 
-                Tuple<List<int>, float[]> x = Fill(strModel, "x", m_log);
-                m_blob_bottom.Reshape(x.Item1);
-                m_blob_bottom.mutable_cpu_data = convert(x.Item2);
+                m_blobQ.LoadFromNumpy(strTestDataPath + "q0.npy");
+                m_blobK.LoadFromNumpy(strTestDataPath + "k0.npy");
+                m_blobV.LoadFromNumpy(strTestDataPath + "v0.npy");
 
-                Tuple<List<int>, float[]> y_grad = Fill(strModel, "grad_y", m_log, "iter_0");                
-                Tuple<List<int>, float[]> x_grad = Fill(strModel, "grad_x", m_log, "iter_0");
-                Tuple<List<int>, float[]> attnBias = Fill(strModel, "attn_bias", m_log);
-                Tuple<List<int>, float[]> attnWt = Fill(strModel, "attn_weight", m_log);
-                Tuple<List<int>, float[]> projBias = Fill(strModel, "attn_proj_bias", m_log);
-                Tuple<List<int>, float[]> projWt = Fill(strModel, "attn_proj_weight", m_log);
+                BottomVec.Clear();
+                BottomVec.Add(m_blobQ);
+                BottomVec.Add(m_blobK);
+                BottomVec.Add(m_blobV);
 
                 layer.Setup(BottomVec, TopVec);
 
-                layer.blobs[0].mutable_cpu_data = convert(attnWt.Item2);
-                layer.blobs[1].mutable_cpu_data = convert(attnBias.Item2);
-                layer.blobs[2].mutable_cpu_data = convert(projWt.Item2);
-                layer.blobs[3].mutable_cpu_data = convert(projBias.Item2);
+                layer.blobs[0].LoadFromNumpy(strTestDataPath + "w_q_weight.npy");
+                layer.blobs[1].LoadFromNumpy(strTestDataPath + "w_q_bias.npy");
+                layer.blobs[2].LoadFromNumpy(strTestDataPath + "w_k_weight.npy");
+                layer.blobs[3].LoadFromNumpy(strTestDataPath + "w_k_bias.npy");
+                layer.blobs[4].LoadFromNumpy(strTestDataPath + "w_v_weight.npy");
+                layer.blobs[5].LoadFromNumpy(strTestDataPath + "w_v_bias.npy");
+                layer.blobs[6].LoadFromNumpy(strTestDataPath + "w_o_weight.npy");
+                layer.blobs[7].LoadFromNumpy(strTestDataPath + "w_o_bias.npy");
 
                 layer.Forward(BottomVec, TopVec);
 
-                m_blob_top.mutable_cpu_diff = convert(y_grad.Item2);
+                // Load the inbound gradients.
+                TopVec[0].LoadFromNumpy(strTestDataPath + "grad_12_output.npy", true);
 
                 layer.Backward(TopVec, new List<bool>() { true }, BottomVec);
-                
+
+                m_blobQexp.LoadFromNumpy(strTestDataPath + "grad_1_q.npy", true);
+                m_blobKexp.LoadFromNumpy(strTestDataPath + "grad_1_k.npy", true);
+                m_blobVexp.LoadFromNumpy(strTestDataPath + "grad_1_v.npy", true);
+
                 // Now, check values
-                float[] rgExpected = x_grad.Item2;
-                float[] rgActual = convertF(m_blob_bottom.mutable_cpu_diff);
-
-                for (int i = 0; i < rgExpected.Length; i++)
-                {
-                    float fExpected = rgExpected[i];
-                    float fActual = rgActual[i];
-                    float fErr = 0.00000001f;
-
-                    m_log.EXPECT_NEAR_FLOAT(fExpected, fActual, fErr, "The values are not as expected!");
-                }
+                verify(BottomVec[0], m_blobQexp, true);
+                verify(BottomVec[1], m_blobKexp, true);
+                verify(BottomVec[2], m_blobVexp, true);
             }
             finally
             {
@@ -308,32 +343,50 @@ namespace MyCaffe.test
             }
         }
 
+        private void verify(Blob<T> b1, Blob<T> b1exp, bool bCompareDiff)
+        {
+            float[] rgExpected = (bCompareDiff) ? convertF(b1exp.mutable_cpu_diff) : convertF(b1exp.mutable_cpu_data);
+            float[] rgActual = (bCompareDiff) ? convertF(b1.mutable_cpu_diff) : convertF(b1.mutable_cpu_data);
+
+            for (int i = 0; i < rgExpected.Length; i++)
+            {
+                float fExpected = rgExpected[i];
+                float fActual = rgActual[i];
+                float fErr = 1e-7f;
+
+                m_log.EXPECT_NEAR_FLOAT(fExpected, fActual, fErr, "The values are not as expected!");
+            }
+        }
+
         /// <summary>
         /// WORK IN PROGRESS
         /// </summary>
-        public void TestGradient(bool bBatch, int nHeads)
+        public void TestGradient(int nBatch, int nHeads)
         {
+            string strTestDataPath = loadTestData();
+            
             LayerParameter p = new LayerParameter(LayerParameter.LayerType.MULTIHEAD_ATTENTION);
             p.multihead_attention_param.heads = nHeads;
-            p.multihead_attention_param.embed = 3;
-            p.multihead_attention_param.block_size = 4;
+            p.multihead_attention_param.embed = 512;
+            p.multihead_attention_param.block_size = 200;
             p.multihead_attention_param.attn_dropout = 0.0;
             p.multihead_attention_param.resid_dropout = 0.0;
             Layer<T> layer = Layer<T>.Create(m_cuda, m_log, p, new CancelEvent());
 
             try
             {
-                string strModel = "gpt-pico-csa";
-                if (nHeads > 1)
-                    strModel += nHeads.ToString();
-                if (bBatch)
-                    strModel += "B";
+                m_log.CHECK(layer.type == LayerParameter.LayerType.MULTIHEAD_ATTENTION, "The layer type is incorrect!");
 
-                m_log.CHECK(layer.type == LayerParameter.LayerType.CAUSAL_SELF_ATTENTION, "The layer type is incorrect!");
+                m_blobQ.LoadFromNumpy(strTestDataPath + "q0.npy");
+                m_blobK.LoadFromNumpy(strTestDataPath + "k0.npy");
+                m_blobV.LoadFromNumpy(strTestDataPath + "v0.npy");
 
-                Tuple<List<int>, float[]> data = Fill(strModel, "x", m_log);
-                m_blob_bottom.Reshape(data.Item1);
-                m_blob_bottom.mutable_cpu_data = convert(data.Item2);
+                BottomVec.Clear();
+                BottomVec.Add(m_blobQ);
+                BottomVec.Add(m_blobK);
+                BottomVec.Add(m_blobV);
+
+                layer.Setup(BottomVec, TopVec);
 
                 GradientChecker<T> checker = new GradientChecker<T>(m_cuda, m_log, 0.01, 0.001);
                 checker.CheckGradient(layer, BottomVec, TopVec);
