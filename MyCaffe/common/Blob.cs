686b991ba560f3eb42ccbafa2afaea1112a7d3f8
Dave Brown <daveb@signalpop.com>
2022-12-27 17:25:52 -0800

    Added Blob Compare

--- MyCaffe/common/Blob.cs
+++ MyCaffe/common/Blob.cs
@@ -990,6 +990,36 @@ namespace MyCaffe.common
             }
         }
 
+        /// <summary>
+        /// Compare the data (or diff) of one blob to another and return true if all items fall within the specified tolerance or not.
+        /// </summary>
+        /// <param name="other">Specifies the other blob to compare.</param>
+        /// <param name="work">Specifies a temporary work blob.</param>
+        /// <param name="bDiff">Specifies to compare the diff.</param>
+        /// <param name="dfTol">Specifies the accepted tolerance.</param>
+        /// <returns>If all data (or diff) values fall within the tolerance, true is returned, otherwise false.</returns>
+        public bool Compare(Blob<T> other, Blob<T> work, bool bDiff = false, double dfTol = 1e-8)
+        {
+            if (count() != other.count())
+                return false;
+
+            work.ReshapeLike(this);
+
+            long h1 = (bDiff) ? gpu_diff : gpu_data;
+            long h2 = (bDiff) ? other.gpu_diff : other.gpu_data;
+
+            m_cuda.sub(count(), h1, h2, work.mutable_gpu_data);
+            double dfMin = work.min_data;
+            if (dfMin < -dfTol)
+                return false;
+
+            double dfMax = work.max_data;
+            if (dfMax > dfTol)
+                return false;
+
+            return true;
+        }
+
 
 #pragma warning disable 1591
 
