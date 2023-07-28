b60a60bb0dfa4cacdc42b1f76d625b973186cab1
Dave Brown <daveb@signalpop.com>
2023-07-28 12:35:55 -0700

    Added LSTM and Split layers.

--- MyCaffe.converter.pytorch/layers/CommonLayers.cs
+++ MyCaffe.converter.pytorch/layers/CommonLayers.cs
@@ -25,12 +25,40 @@ namespace MyCaffe.converter.pytorch.layers
             if (gen == GENERATE.DEFINITION)
                 strCode += "        self." + m_layer.name + " = nn.Linear(in_features=" + nInFeatures + ", out_features=" + nOutFeatures + ", bias=" + m_layer.inner_product_param.bias_term.ToString() + ")" + Environment.NewLine;
             else if (gen == GENERATE.INITWEIGHTS)
-                strCode += initWeights(m_layer.name, m_layer.inner_product_param.bias_term, m_layer.inner_product_param.weight_filler, m_layer.inner_product_param.bias_filler);
+                strCode += initWeights("", m_layer.name, m_layer.inner_product_param.bias_term, m_layer.inner_product_param.weight_filler, m_layer.inner_product_param.bias_filler);
             else if (gen == GENERATE.FORWARD)
             {
                 strCode += "        " + m_inputs.AsText + " = " + m_inputs.AsText + ".view(" + m_inputs.AsText + ".size(0), -1)" + Environment.NewLine;
                 strCode += "        " + m_outputs.AsText + " = self." + m_layer.name + "(" + m_inputs.AsText + ")" + Environment.NewLine;
             }
+
+            return strCode;
+        }
+    }
+
+    class LSTMLayerInfo : LayerInfo /** @private */
+    {
+        public LSTMLayerInfo(LayerParameter layer, VariableCollection inputs) : base(layer, inputs)
+        {
+        }
+
+        public override string Generate(GENERATE gen)
+        {
+            string strCode = "";
+            if (gen == GENERATE.CLASSES)
+            {
+            }
+            if (gen == GENERATE.DEFINITION)
+            {
+                string strStateSize = m_layer.recurrent_param.num_output.ToString();
+                strCode += "        self." + m_layer.name + " = LSTM(input_dim=" + strStateSize + ", hidden_size=" + strStateSize + ", num_layers=" + m_layer.recurrent_param.num_layers.ToString() + ", dropout=" + m_layer.recurrent_param.dropout_ratio.ToString() + ", batch_first=" + m_layer.recurrent_param.batch_first.ToString() + ")" + Environment.NewLine;
+            }
+            else if (gen == GENERATE.INITWEIGHTS)
+            {
+                strCode += initWeights("", m_layer.name, true, m_layer.recurrent_param.weight_filler, m_layer.recurrent_param.bias_filler);
+            }
+            else if (gen == GENERATE.FORWARD)
+                strCode += "        " + m_outputs.AsText + " = self." + m_layer.name + "(" + m_inputs.AsText + ")" + Environment.NewLine;
 
             return strCode;
         }
@@ -63,6 +91,60 @@ namespace MyCaffe.converter.pytorch.layers
             }
             else if (gen == GENERATE.FORWARD)
                 strCode += "        " + m_outputs.AsText + " = torch.concat(" + m_inputs.AsText + ", dim=0)" + Environment.NewLine;
+
+            return strCode;
+        }
+    }
+
+    class SplitLayerInfo : LayerInfo /** @private */
+    {
+        static int m_nGenerateCount = 0;
+
+        public SplitLayerInfo(LayerParameter layer, VariableCollection inputs) : base(layer, inputs)
+        {
+            m_outputs = m_inputs.Clone();
+            m_outputs[0].Name = layer.top[0];
+            m_outputs.Add(layer.top[1], m_inputs[0].Shape);
+        }
+
+        public override string Generate(GENERATE gen)
+        {
+            string strCode = "";
+            if (gen == GENERATE.CLASSES)
+            {
+                strCode += generateSplitClass(m_layer);
+            }
+            else if (gen == GENERATE.DEFINITION)
+            {
+                strCode += "#       self." + m_layer.name + " = Split(" + m_inputs.AsText + ")" + Environment.NewLine;
+            }
+            else if (gen == GENERATE.INITWEIGHTS)
+            {
+            }
+            else if (gen == GENERATE.FORWARD)
+                strCode += "        " + m_outputs.AsText + " = self." + m_layer.name + "(" + m_inputs.AsText + ")" + Environment.NewLine;
+
+            return strCode;
+        }
+
+        private string generateSplitClass(LayerParameter p)
+        {
+            string strCode = "";
+
+            if (m_nGenerateCount > 0)
+                return strCode;
+
+            strCode += "class Split(nn.Module):" + Environment.NewLine;
+            strCode += "    def __init__(self):" + Environment.NewLine;
+            strCode += "        super(Split, self).__init__()" + Environment.NewLine;
+            strCode += Environment.NewLine;
+            strCode += "    def forward(self, x: torch.Tensor) -> torch.Tensor:" + Environment.NewLine;
+            strCode += "        x1 = x.detach().clone()" + Environment.NewLine;
+            strCode += "        x2 = x.detach().clone()" + Environment.NewLine;
+            strCode += "        return x1, x2" + Environment.NewLine;
+            strCode += Environment.NewLine;
+
+            m_nGenerateCount++;
 
             return strCode;
         }
