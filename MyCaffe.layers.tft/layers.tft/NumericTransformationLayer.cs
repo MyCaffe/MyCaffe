bea7b3e9de97d9bbdddc018aa77dbf768ef8dbe9
Dave Brown <daveb@signalpop.com>
2023-09-10 14:39:21 -0700

    Fixed bug in spatial dim.

--- MyCaffe.layers.tft/layers.tft/NumericTransformationLayer.cs
+++ MyCaffe.layers.tft/layers.tft/NumericTransformationLayer.cs
@@ -90,7 +90,8 @@ namespace MyCaffe.layers.tft
         {
             int nOffset = (colBottom[0].num_axes == 2) ? 1 : 2;
             int nDim = colBottom[0].count(0, nOffset);
-            int nSpatialDim = colBottom[0].count(colBottom[0].num_axes - 1);
+            int nNumInput = (int)m_param.numeric_trans_param.num_input;
+            int nSpatialDim = colBottom[0].count(colBottom[0].num_axes - 1) / nNumInput;
             List<int> rgShape = new List<int>() { nDim, nSpatialDim };
             Blob<T> blobBtm = null;
 
@@ -99,7 +100,7 @@ namespace MyCaffe.layers.tft
             m_rgIpTop.Clear();
             m_rgIpTop.Add(colTop[0]);
 
-            for (int i = 0; i < m_param.numeric_trans_param.num_input; i++)
+            for (int i = 0; i < nNumInput; i++)
             {
                 blobBtm = new Blob<T>(m_cuda, m_log);
                 blobBtm.Reshape(rgShape);
