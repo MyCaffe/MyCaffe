1985bb418b7e4d5718c123d60427ab87ff105d74
Dave Brown <daveb@signalpop.com>
2023-11-13 06:53:28 -0800

    Added MyCaffe.layers.ptst for PatchTST

---
+++ MyCaffe.layers.ptst/LayerFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCaffe.basecode;
+using MyCaffe.common;
+using MyCaffe.db.image;
+using MyCaffe.param;
+
+/// <summary>
+/// The MyCaffe.layers.ptst namespace contains all PatchTST related layers.
+/// </summary>
+namespace MyCaffe.layers.ptst
+{
+    /// <summary>
+    /// The LayerFactor is responsible for creating all layers implemented in the MyCaffe.layers.ptst namespace.
+    /// </summary>
+    public class LayerFactory : ILayerCreator
+    {
+        /// <summary>
+        /// Create the layers when using the <i>double</i> base type.
+        /// </summary>
+        /// <param name="cuda">Specifies the connection to the low-level CUDA interfaces.</param>
+        /// <param name="log">Specifies the output log.</param>
+        /// <param name="p">Specifies the layer parameter.</param>
+        /// <param name="evtCancel">Specifies the cancellation event.</param>
+        /// <param name="db">Specifies an interface to the in-memory database, who's use is optional.</param>
+        /// <returns>If supported, the layer is returned, otherwise <i>null</i> is returned.</returns>
+        public Layer<double> CreateDouble(CudaDnn<double> cuda, Log log, LayerParameter p, CancelEvent evtCancel, IXDatabaseBase db)
+        {
+            switch (p.type)
+            {
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Create the layers when using the <i>float</i> base type.
+        /// </summary>
+        /// <param name="cuda">Specifies the connection to the low-level CUDA interfaces.</param>
+        /// <param name="log">Specifies the output log.</param>
+        /// <param name="p">Specifies the layer parameter.</param>
+        /// <param name="evtCancel">Specifies the cancellation event.</param>
+        /// <param name="db">Specifies an interface to the in-memory database, who's use is optional.</param>
+        /// <returns>If supported, the layer is returned, otherwise <i>null</i> is returned.</returns>
+        public Layer<float> CreateSingle(CudaDnn<float> cuda, Log log, LayerParameter p, CancelEvent evtCancel, IXDatabaseBase db)
+        {
+            switch (p.type)
+            {
+                default:
+                    return null;
+            }
+        }
+    }
+}
