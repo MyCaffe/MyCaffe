4195129d76c923627d2e8de5683a152b04bc4a96
Dave Brown <daveb@signalpop.com>
2023-11-17 13:39:06 -0800

    Updated to latest build.

--- MyCaffe.test/Properties/AssemblyInfo.cs
+++ MyCaffe.test/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.12.2.65")]
-[assembly: AssemblyFileVersion("1.12.2.65")]
+[assembly: AssemblyVersion("1.12.2.71")]
+[assembly: AssemblyFileVersion("1.12.2.71")]
