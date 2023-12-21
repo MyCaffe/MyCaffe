afe550475d73eb276374dce5536d4b826d581336
Dave Brown <daveb@signalpop.com>
2023-12-21 11:47:57 -0800

    Removed unused using.

--- MyCaffe.basecode/ColorMapper.cs
+++ MyCaffe.basecode/ColorMapper.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
 
 namespace MyCaffe.basecode
 {
