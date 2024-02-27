4ed320d87e4d903d7a416720dc29938c89fd65c5
Dave Brown <daveb@signalpop.com>
2024-02-26 21:27:05 -0800

    Added parameter for number of heads.

--- MyCaffe/common/CudaDnn.cs
+++ MyCaffe/common/CudaDnn.cs
@@ -6142,19 +6142,20 @@ namespace MyCaffe.common
         /// <param name="nCount">Specifies the total number of items in the input (and output).</param>
         /// <param name="nBatch">Specifies the batch size of items (e.g., num)</param>
         /// <param name="nSeqLen">Specifies the sequence length used.</param>
+        /// <param name="nHeads">Specifies the number of heads used.</param>
         /// <param name="nDim">Specifies the spatial dimentions of the inner data.</param>
         /// <param name="fTheta">Optionally, specifies the theta value used in the rope calculation (default = 10000f).</param>
         /// <returns>The handle to the LayerNorm configuration.  This handle is used with all other layer norm functions.</returns>
-        public long CreateRope(int nGpuID, int nCount, int nBatch, int nSeqLen, int nDim, float fTheta = 10000.0f)
+        public long CreateRope(int nGpuID, int nCount, int nBatch, int nSeqLen, int nHeads, int nDim, float fTheta = 10000.0f)
         {
             if (m_dt == DataType.DOUBLE)
             {
-                double[] rg = m_cuda.RunDoubleEx2((int)m_hKernel, (int)CUDAFN.CUDA_CREATE_ROPE, m_param.AsDouble(fTheta), m_param.AsLong(nGpuID, nCount, nBatch, nSeqLen, nDim));
+                double[] rg = m_cuda.RunDoubleEx2((int)m_hKernel, (int)CUDAFN.CUDA_CREATE_ROPE, m_param.AsDouble(fTheta), m_param.AsLong(nGpuID, nCount, nBatch, nSeqLen, nHeads, nDim));
                 return (long)rg[0];
             }
             else
             {
-                float[] rg = m_cuda.RunFloatEx2((int)m_hKernel, (int)CUDAFN.CUDA_CREATE_ROPE, m_param.AsFloat(fTheta), m_param.AsLong(nGpuID, nCount, nBatch, nSeqLen, nDim));
+                float[] rg = m_cuda.RunFloatEx2((int)m_hKernel, (int)CUDAFN.CUDA_CREATE_ROPE, m_param.AsFloat(fTheta), m_param.AsLong(nGpuID, nCount, nBatch, nSeqLen, nHeads, nDim));
                 return (long)rg[0];
             }
         }
